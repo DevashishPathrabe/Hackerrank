6abe48ae8868ec935ea2434a2958eeb49f14ce46
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-22 17:33:40 +0530

    Vector-Sort
    Here is my Vector-Sort Hackerrank Solution...

---
+++ HackerRank Solutions/C++/Vector-Sort.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> arr;
+    int N;
+    scanf("%d", &N);
+    for(int i=0; i<N; i++){
+        int temp;
+        scanf("%d", &temp);
+        arr.push_back(temp);
+    }
+    sort(arr.begin(), arr.end());
+    for(int i=0; i<N; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
