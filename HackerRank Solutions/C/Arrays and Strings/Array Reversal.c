68432f4c0eb55815d05415c2f4fbe668c45f74ad
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-23 14:33:06 +0530

    Array Reversal

---
+++ HackerRank Solutions/C/Arrays and Strings/Array Reversal.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(int arr_i=0; arr_i<num; arr_i++){
+       scanf("%d",&arr[arr_i]);
+    }
+    for(int arr_i=num-1; arr_i>=0; arr_i--){
+        printf("%d ",arr[arr_i]);
+  }
+    return 0;
+}
