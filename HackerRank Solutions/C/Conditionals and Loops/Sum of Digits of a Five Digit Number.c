b48b628e45ad5fbc99c051b780b6394cab787f4e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-23 13:53:03 +0530

    Sum of Digits of a Five Digit Number

---
+++ HackerRank Solutions/C/Conditionals and Loops/Sum of Digits of a Five Digit Number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {	
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    while(n){
+        sum += n % 10;
+        n /= 10;
+    }
+    printf("%d", sum);
+    return 0;
+}
