46eb46c6625b7c04cf7bc399b3cc835b1b844eec
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-24 10:46:54 +0530

    Collections.OrderedDict()
    Here is my Collections.OrderedDict() Hackerrank Solution...

---
+++ HackerRank Solutions/Python/Collections/Collections.OrderedDict().py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+order = OrderedDict()
+for _ in range(int(input())):
+    item, space, price = input().rpartition(' ')
+    order[item] = order.get(item, 0) + int(price)
+for item, price in order.items():
+    print(item, price)
