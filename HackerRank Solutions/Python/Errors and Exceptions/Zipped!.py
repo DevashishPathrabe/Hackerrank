720dbec0db0fdbd3e2976a8d057fc6739e460236
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-24 11:40:53 +0530

    Delete Zipped!.py

--- HackerRank Solutions/Python/Errors and Exceptions/Zipped!.py
+++
@@ -1,6 +0,0 @@
-N, X = map(int,input().split())
-listOfMarks = []
-for subject in range(X):
-    listOfMarks += [map(float,input().split())]   
-for studentMarks in zip(*listOfMarks):    
-    print (sum(studentMarks)/X)
