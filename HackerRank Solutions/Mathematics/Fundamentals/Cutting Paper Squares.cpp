cd3fd6856171c5d3ddbc0bcc62d89cdc2c107236
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-25 09:59:15 +0530

    Cutting Paper Squares
    Here is my Cutting Paper Squares Hackerrank Solution...

---
+++ HackerRank Solutions/Mathematics/Fundamentals/Cutting Paper Squares.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int n, m, noOfCuts;
+    cin >> n >> m;
+    noOfCuts = m*n-1;
+    cout << noOfCuts << endl;
+    return 0;
+}
