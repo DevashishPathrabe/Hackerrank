088cd135e73067b0ae5f293ba9d195c3b10048f0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-25 10:02:34 +0530

    Maximum Draws
    Here is my Maximum Draws Hackerrank Solution...

---
+++ HackerRank Solutions/Mathematics/Fundamentals/Maximum Draws.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int T, socks;
+  cin >> T;
+  while (T--) {
+    cin >> socks;
+    cout << socks + 1 << endl;
+  }
+  return 0;
+}
