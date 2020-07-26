06c92ad6f9b1f08d203dad678d9b77ff0346abe5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-26 18:26:16 +0530

    Simple Array Sum
    Here is my Simple Array Sum Hackerrank Solution in C++...

---
+++ HackerRank Solutions/Algorithms/Warmup/Simple Array Sum/Simple Array Sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
