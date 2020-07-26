8692c09b0f39ad9f562a4b3c0e70bdf00a23d706
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-26 10:49:43 +0530

    Create A Very Big Sum.cpp

---
+++ HackerRank Solutions/Algorithms/A Very Big Sum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    long long int sum = 0;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum << endl;
+    return 0;
+}
