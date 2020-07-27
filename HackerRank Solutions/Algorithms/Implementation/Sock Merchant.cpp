4be27239a8526e6fcff2b9e5ac1ceea8bfd0e34e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-27 21:25:46 +0530

    Create Sock Merchant.cpp

---
+++ HackerRank Solutions/Algorithms/Implementation/Sock Merchant.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, totalpairs = 0, arr[102], count[102] = {};
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+        count[arr[i]]++;
+    }
+    for(int i=0; i<=100; i++){
+        if(count[i] != 0){
+            totalpairs += count[i]/2;
+        }
+    }
+    cout << totalpairs << endl;
+    return 0;
+}
