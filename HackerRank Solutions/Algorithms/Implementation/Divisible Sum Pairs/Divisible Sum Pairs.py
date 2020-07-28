46137a4e12a6ca7290b2cc81789602721435ae52
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-28 20:56:33 +0530

    Update Divisible Sum Pairs.py

--- HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.py
+++ HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.py
@@ -11,7 +11,7 @@ def divisibleSumPairs(n, k, ar):
     count = 0
     for i in range(0,n):
         for j in range(i+1,n):
-            if((ar[i] + ar[j]) % k == 0):
+            if((ar[i]+ar[j]) % k == 0):
                 count += 1;
     return (count);
 
