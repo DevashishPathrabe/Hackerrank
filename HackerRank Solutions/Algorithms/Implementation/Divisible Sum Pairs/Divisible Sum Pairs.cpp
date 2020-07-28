c95e3ce45ce4552b537c1db8aec5efd2a7e50066
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-28 20:46:51 +0530

    Update Divisible Sum Pairs.cpp

--- HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    int i, j ,k , n, pairs=0;
+    int k , n, pairs=0;
     cin >> n >> k;
     vector<int> a(n);
-    for(i = 0; i < n ; i++){
+    for(int i=0; i<n ; i++){
         cin >> a[i];
     }
-    for(i = 0; i < n ; i++){
-        for(j = 0; j < n ; j++){
+    for(int i=0; i<n ; i++){
+        for(int j=0; j<n; j++){
             if(i < j){
-                if((a[i] + a[j]) % k == 0){
+                if((a[i]+a[j]) % k == 0){
                     pairs++;
                 }
             }
         }
     }
-    cout<<pairs;
+    cout << pairs << endl;
     return 0;
 }
