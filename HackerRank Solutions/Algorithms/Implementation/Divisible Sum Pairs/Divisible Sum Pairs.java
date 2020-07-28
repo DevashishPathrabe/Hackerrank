b24e20e9196a92d9be7e30c193d0ea4e4cb32ac5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-28 20:56:22 +0530

    Update Divisible Sum Pairs.java

--- HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.java
+++ HackerRank Solutions/Algorithms/Implementation/Divisible Sum Pairs/Divisible Sum Pairs.java
@@ -11,15 +11,14 @@ public class Solution {
     // Complete the divisibleSumPairs function below.
     static int divisibleSumPairs(int n, int k, int[] ar) {
         int count = 0;
-        for(int i=0;i<n;i++){
-            for (int j=i+1;j<n;j++){
-                if((ar[i] + ar[j]) % k == 0){
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if((ar[i]+ar[j]) % k == 0){
                     count += 1;
                 }
             }
         }
         return (count);
-
     }
 
     private static final Scanner scanner = new Scanner(System.in);
