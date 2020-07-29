1891c94c1e6b72c2f7bf7e411acf1c6428070c5e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-29 21:00:38 +0530

    Create Save the Prisoner!.cpp

---
+++ HackerRank Solutions/Algorithms/Implementation/Save the Prisoner!.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n, m, s, chairNumber;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        cin >> n >> m >> s;
+        chairNumber = (s + m - 1) % n;
+        if(chairNumber == 0){
+            cout << n << endl;
+        } else{
+            cout << chairNumber << endl;
+        }
+    }
+    return 0;
+}
