b783be8a622571fa4ec6491d97736d7bb23377c4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-30 11:36:53 +0530

    Jumping on the Clouds: Revisited
    Here is my Jumping on the Clouds: Revisited Hackerrank Solution...

---
+++ HackerRank Solutions/Algorithms/Implementation/Jumping on the Clouds  Revisited/Jumping on the Clouds Revisited.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c, k):
+    i = 0;
+    e = 100;
+    i = (i+k )% len(c);
+    e -= 1+c[i]*2;
+    while(i != 0):
+        i = (i+k) % len(c);
+        e -= 1+c[i]*2;
+    return e;
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
