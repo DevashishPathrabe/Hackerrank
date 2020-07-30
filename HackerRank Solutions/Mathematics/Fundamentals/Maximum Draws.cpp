274708c82371a411c89560020114ec022e5c51ac
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-30 19:33:23 +0530

    Update Maximum Draws.cpp

--- HackerRank Solutions/Mathematics/Fundamentals/Maximum Draws.cpp
+++ HackerRank Solutions/Mathematics/Fundamentals/Maximum Draws.cpp
@@ -1,12 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main() {
-  int T, socks;
-  cin >> T;
-  while (T--) {
-    cin >> socks;
-    cout << socks + 1 << endl;
-  }
-  return 0;
+/*
+ * Complete the maximumDraws function below.
+ */
+int maximumDraws(int n) {
+    /*
+     * Write your code here.
+     */
+    return(n+1);
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int n;
+        cin >> n;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        int result = maximumDraws(n);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
 }
