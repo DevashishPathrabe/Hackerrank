59f99bedba7470a93dd18143b355fb5e6d88299c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-30 20:17:23 +0530

    Update Minimum Height Triangle.cpp

--- HackerRank Solutions/Mathematics/Fundamentals/Minimum Height Triangle.cpp
+++ HackerRank Solutions/Mathematics/Fundamentals/Minimum Height Triangle.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-int main()
-{
-    int base, area, height;
+int lowestTriangle(int base, int area){
+    // Complete this function
+    return(ceil((float)(2 * area)/base));
+}
+
+int main() {
+    int base;
+    int area;
     cin >> base >> area;
-    height = ceil((float)(2 * area) / base);
+    int height = lowestTriangle(base, area);
     cout << height << endl;
     return 0;
 }
