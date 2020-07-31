32ce37beff61d47ab63d6b103ced10ceab1eae38
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-31 19:57:49 +0530

    Create Constructing a Number.c

---
+++ HackerRank Solutions/Mathematics/Constructing a Number.c
@@ -0,0 +1,130 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the canConstruct function below.
+
+// Please either make the string static or allocate on the heap. For example,
+// static char str[] = "hello world";
+// return str;
+//
+// OR
+//
+// char* str = "hello world";
+// return str;
+//
+char* canConstruct(int a_count, int* a) {
+    // Return "Yes" or "No" denoting whether you can construct the required number.
+    long int i,sum=0;
+    for(i=0;i<a_count;i++){
+        sum=sum+a[i];
+    }
+    if(sum%3 == 0){
+        return "Yes";
+    }
+    return "No";
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char* n_endptr;
+        char* n_str = readline();
+        int n = strtol(n_str, &n_endptr, 10);
+
+        if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char** a_temp = split_string(readline());
+
+        int* a = malloc(n * sizeof(int));
+
+        for (int i = 0; i < n; i++) {
+            char* a_item_endptr;
+            char* a_item_str = *(a_temp + i);
+            int a_item = strtol(a_item_str, &a_item_endptr, 10);
+
+            if (a_item_endptr == a_item_str || *a_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            *(a + i) = a_item;
+        }
+
+        int a_count = n;
+
+        char* result = canConstruct(a_count, a);
+
+        fprintf(fptr, "%s\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
