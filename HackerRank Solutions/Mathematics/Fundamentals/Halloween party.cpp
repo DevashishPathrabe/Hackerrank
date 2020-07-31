25730b6bc0b9c61b89aad6e1990add579d996d98
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-31 18:59:15 +0530

    Update Halloween party.cpp

--- HackerRank Solutions/Mathematics/Fundamentals/Halloween party.cpp
+++ HackerRank Solutions/Mathematics/Fundamentals/Halloween party.cpp
@@ -1,14 +1,36 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+/*
+ * Complete the halloweenParty function below.
+ */
+long halloweenParty(long int k) {
+    /*
+     * Write your code here.
+     */
+    return ((k-k/2)*(k/2));
+}
 
 int main()
 {
-    long int T, K, Chocolate;
-    cin >> T;
-    for(int i=0; i<T;i++){
-        cin >> K;
-        cout << (K-K/2)*(K/2) << endl;
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        int k;
+        cin >> k;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        long result = halloweenParty(k);
+
+        fout << result << "\n";
     }
+
+    fout.close();
+
     return 0;
 }
-
