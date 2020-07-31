c5532c47e7471933163e6c340913ef8b1b997867
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-07-31 15:06:31 +0530

    Create Summing the N Series.java

---
+++ HackerRank Solutions/Mathematics/Fundamentals/Summing the N Series.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    /*
+     * Complete the summingSeries function below.
+     */
+    static int summingSeries(long n) {
+        /*
+         * Write your code here.
+         */
+        return (int)(((n%1000000007)*(n%1000000007))%1000000007);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(scanner.nextLine().trim());
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            long n = Long.parseLong(scanner.nextLine().trim());
+
+            int result = summingSeries(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+    }
+}
