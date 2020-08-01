21f5122258127ddce47d50e20611d60cce92e2e2
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-01 10:17:39 +0530

    Closest Number
    Here is my Closest Number Hackerrank Solution...

---
+++ HackerRank Solutions/Mathematics/Number Theory/Closest Number/Closest Number.c
@@ -0,0 +1,118 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+/*
+ * Complete the closestNumber function below.
+ */
+int closestNumber(int a, int b, int x) {
+    /*
+     * Write your code here.
+     */
+    int leftDifference = floor(pow(a,b)/x)*x;
+    int rightDifference = ceil(pow(a,b)/x)*x;
+    if(pow(a,b) - leftDifference < rightDifference - pow(a,b)){
+        return (leftDifference);
+    }
+    return (rightDifference);
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char** abx = split_string(readline());
+
+        char* a_endptr;
+        char* a_str = abx[0];
+        int a = strtol(a_str, &a_endptr, 10);
+
+        if (a_endptr == a_str || *a_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char* b_endptr;
+        char* b_str = abx[1];
+        int b = strtol(b_str, &b_endptr, 10);
+
+        if (b_endptr == b_str || *b_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char* x_endptr;
+        char* x_str = abx[2];
+        int x = strtol(x_str, &x_endptr, 10);
+
+        if (x_endptr == x_str || *x_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        int result = closestNumber(a, b, x);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
