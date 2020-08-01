4e03280537f0c663fca36657c2a217010861f4ce
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-01 10:14:39 +0530

    Update Closest Number.cpp

--- HackerRank Solutions/Mathematics/Number Theory/Closest Number.cpp
+++ HackerRank Solutions/Mathematics/Number Theory/Closest Number.cpp
@@ -1,21 +1,79 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+vector<string> split_string(string);
+
+/*
+ * Complete the closestNumber function below.
+ */
+int closestNumber(int a, int b, int x) {
+    /*
+     * Write your code here.
+     */
+    int leftDifference = floor(pow(a,b)/x)*x;
+    int rightDifference = ceil(pow(a,b)/x)*x;
+    if(pow(a,b) - leftDifference < rightDifference - pow(a,b)){
+        return (leftDifference);
+    }
+    return (rightDifference);
+}
 
 int main()
 {
-    long int T, a, b, x, temp;
-    cin >> T;
-    for (int i = 0; i < T; i++){
-        cin >> a >> b >> x;
-        temp = pow(a,b);
-        int value = temp / x;
-        int tempOnLeft = x * value;
-        int tempOnRight = x * (value + 1);
-        if ((temp - tempOnLeft) < (tempOnRight - temp)) {
-            cout << tempOnLeft << endl;
-        } else {
-            cout << tempOnRight << endl;
-        }
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string abx_temp;
+        getline(cin, abx_temp);
+
+        vector<string> abx = split_string(abx_temp);
+
+        int a = stoi(abx[0]);
+
+        int b = stoi(abx[1]);
+
+        int x = stoi(abx[2]);
+
+        int result = closestNumber(a, b, x);
+
+        fout << result << "\n";
     }
+
+    fout.close();
+
     return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
 }
