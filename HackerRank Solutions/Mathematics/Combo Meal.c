70d81b82d252c546aae431380e261c1e23b967e3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-01 10:50:30 +0530

    Create Combo Meal.c

---
+++ HackerRank Solutions/Mathematics/Combo Meal.c
@@ -0,0 +1,111 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+// Complete the profit function below.
+int profit(int b, int s, int c) {
+    // Return the fixed profit.
+    return ((b+s) - c);
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* t_endptr;
+    char* t_str = readline();
+    int t = strtol(t_str, &t_endptr, 10);
+
+    if (t_endptr == t_str || *t_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        char** bsc = split_string(readline());
+
+        char* b_endptr;
+        char* b_str = bsc[0];
+        int b = strtol(b_str, &b_endptr, 10);
+
+        if (b_endptr == b_str || *b_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char* s_endptr;
+        char* s_str = bsc[1];
+        int s = strtol(s_str, &s_endptr, 10);
+
+        if (s_endptr == s_str || *s_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        char* c_endptr;
+        char* c_str = bsc[2];
+        int c = strtol(c_str, &c_endptr, 10);
+
+        if (c_endptr == c_str || *c_endptr != '\0') { exit(EXIT_FAILURE); }
+
+        int result = profit(b, s, c);
+
+        fprintf(fptr, "%d\n", result);
+    }
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
