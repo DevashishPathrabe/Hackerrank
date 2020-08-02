2e643a4c41cdbf5a71676aed90186faa2b4333c6
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-02 19:11:39 +0530

    Update Reverse a doubly linked list.java

--- HackerRank Solutions/Data Structures/Reverse a doubly linked list.java
+++ HackerRank Solutions/Data Structures/Reverse a doubly linked list.java
@@ -1,91 +1,118 @@
-#include <bits/stdc++.h>
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-using namespace std;
+public class Solution {
 
-class DoublyLinkedListNode {
-    public:
-        int data;
-        DoublyLinkedListNode *next;
-        DoublyLinkedListNode *prev;
+    static class DoublyLinkedListNode {
+        public int data;
+        public DoublyLinkedListNode next;
+        public DoublyLinkedListNode prev;
 
-        DoublyLinkedListNode(int node_data) {
-            this->data = node_data;
-            this->next = nullptr;
-            this->prev = nullptr;
+        public DoublyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+            this.prev = null;
         }
-};
+    }
 
-class DoublyLinkedList {
-    public:
-        DoublyLinkedListNode *head;
-        DoublyLinkedListNode *tail;
+    static class DoublyLinkedList {
+        public DoublyLinkedListNode head;
+        public DoublyLinkedListNode tail;
 
-        DoublyLinkedList() {
-            this->head = nullptr;
-            this->tail = nullptr;
+        public DoublyLinkedList() {
+            this.head = null;
+            this.tail = null;
         }
 
-        void insert_node(int node_data) {
-            DoublyLinkedListNode* node = new DoublyLinkedListNode(node_data);
+        public void insertNode(int nodeData) {
+            DoublyLinkedListNode node = new DoublyLinkedListNode(nodeData);
 
-            if (!this->head) {
-                this->head = node;
+            if (this.head == null) {
+                this.head = node;
             } else {
-                this->tail->next = node;
-                node->prev = this->tail;
+                this.tail.next = node;
+                node.prev = this.tail;
             }
 
-            this->tail = node;
-        }
-};
-
-void print_doubly_linked_list(DoublyLinkedListNode* node, string sep, ofstream& fout) {
-    while (node) {
-        fout << node->data;
-
-        node = node->next;
-
-        if (node) {
-            fout << sep;
+            this.tail = node;
         }
     }
-}
 
-void free_doubly_linked_list(DoublyLinkedListNode* node) {
-    while (node) {
-        DoublyLinkedListNode* temp = node;
-        node = node->next;
+    public static void printDoublyLinkedList(DoublyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
 
-        free(temp);
-    }
-}
+            node = node.next;
 
-// Complete the reverse function below.
-
-/*
- * For your reference:
- *
- * DoublyLinkedListNode {
- *     int data;
- *     DoublyLinkedListNode* next;
- *     DoublyLinkedListNode* prev;
- * };
- *
- */
-DoublyLinkedListNode* reverse(DoublyLinkedListNode* head) {
-    if(head == NULL){
-        return NULL;
-    }
-    while(head != NULL){
-        DoublyLinkedListNode *current = head->prev;
-        head->prev = head->next;
-        head->next = current;
-        if(head->prev == NULL){
-            return head;
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
         }
-        head = head->prev;
     }
-    return head;
-}
 
-int main()
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * DoublyLinkedListNode {
+     *     int data;
+     *     DoublyLinkedListNode next;
+     *     DoublyLinkedListNode prev;
+     * }
+     *
+     */
+    static DoublyLinkedListNode reverse(DoublyLinkedListNode head) {
+        if(head == null){
+            return null;
+        }
+        while(head != null){
+            DoublyLinkedListNode current = head.prev;
+            head.prev = head.next;
+            head.next = current;
+            if(head.prev == null){
+                return head;
+            }
+            head = head.prev;
+        }
+        return head;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            DoublyLinkedList llist = new DoublyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            DoublyLinkedListNode llist1 = reverse(llist.head);
+
+            printDoublyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
