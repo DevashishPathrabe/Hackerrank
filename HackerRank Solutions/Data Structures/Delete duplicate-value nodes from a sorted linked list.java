035df5aed32defafa5b46b5bf34a47d225d72244
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-03 20:49:41 +0530

    Update Delete duplicate-value nodes from a sorted linked list.java

--- HackerRank Solutions/Data Structures/Delete duplicate-value nodes from a sorted linked list.java
+++ HackerRank Solutions/Data Structures/Delete duplicate-value nodes from a sorted linked list.java
@@ -78,3 +78,34 @@ public class Solution {
     }
 
     private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llistCount; i++) {
+                int llistItem = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist.insertNode(llistItem);
+            }
+
+            SinglyLinkedListNode llist1 = removeDuplicates(llist.head);
+
+            printSinglyLinkedList(llist1, " ", bufferedWriter);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
