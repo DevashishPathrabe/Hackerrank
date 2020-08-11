fdfb3e7e5c7d74e431c5fb95358cace3e2345485
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-11 20:55:11 +0530

    Update Flatland Space Stations.py

--- HackerRank Solutions/Algorithms/Flatland Space Stations.py
+++ HackerRank Solutions/Algorithms/Flatland Space Stations.py
@@ -11,8 +11,8 @@ def flatlandSpaceStations(n, c):
     c = sorted(c)
     distance = c[0]
     for i in range(1, len(c)):
-        distance = max(distance, (c[i] - c[i-1])//2)
-    distance = max(distance, n-1 - c[-1])
+        distance = max(distance, (c[i] - c[i-1]) // 2)
+    distance = max(distance, n-1-c[-1])
     return distance
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
