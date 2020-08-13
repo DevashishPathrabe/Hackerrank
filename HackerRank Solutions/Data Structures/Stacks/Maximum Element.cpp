9fb7b36dbaed2de9bdf1db79f313b952ade95110
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-13 20:11:57 +0530

    Create Maximum Element.cpp

---
+++ HackerRank Solutions/Data Structures/Stacks/Maximum Element.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack <int> stack;
+    int N, x, query;
+    cin >> N;
+    while(N--){    
+        cin >> query;
+        switch(query){
+            case 1:
+                cin >> x;
+                if(stack.empty()){
+                    stack.push(x);
+                } else{
+                    stack.push(max(x,stack.top()));
+                }
+                break;
+            case 2:
+                if(!stack.empty()){
+                    stack.pop();
+                }
+                break;
+            case 3:
+                cout << stack.top() << endl;
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
