2fbdedb1fd6bdc651e782ab4c783b1174369e91d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-14 20:33:51 +0530

    Update Concatenate.py

--- HackerRank Solutions/Python/Concatenate.py
+++ HackerRank Solutions/Python/Concatenate.py
@@ -1,5 +1,5 @@
 import numpy
 N, M, P = map(int, input().split())
-A = numpy.array([[1,2] for i in range(N)] )
-B = numpy.array([[3,4] for i in range(M)] )
-print (numpy.concatenate((A, B),axis = 0))
+array_1 = numpy.array([[1,2] for i in range(N)] )
+array_2 = numpy.array([[3,4] for i in range(M)] )
+print (numpy.concatenate((array_1, array_2), axis = 0))
