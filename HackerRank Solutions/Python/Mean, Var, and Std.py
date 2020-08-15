1cf02e2517a96f5830b605b50e94614cc3a8c6d3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-08-15 14:58:59 +0530

    Delete Mean, Var, and Std.py

--- HackerRank Solutions/Python/Mean, Var, and Std.py
+++
@@ -1,7 +0,0 @@
-import numpy
-numpy.set_printoptions(legacy = '1.13')
-N, M = map(int, input().split())
-array = numpy.array([input().split() for i in range(N)], int)
-print (numpy.mean(array, axis = 1))
-print (numpy.var(array, axis = 0))
-print (numpy.std(array, axis = None))
