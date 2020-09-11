4816ddc3ea1cdba1296ac22483de143993348bc9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-11 19:33:04 +0530

    Insertion Sort - Part 2
    Here is my Insertion Sort - Part 2 Hackerrank Solution...

---
+++ HackerRank Solutions/Algorithms/Sorting/Insertion Sort - Part 2/Insertion Sort - Part 2.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the insertionSort2 function below.
+    static void insertionSort2(int n, int[] arr) {
+        for(int i=0; i<n-1; i++){
+            int j = i+1;
+            while(j > 0){
+                if(arr[j] < arr[j-1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    j--;
+                } else{
+                    break;
+                }
+            }
+            printArray(arr);
+        }
+    }
+    static void printArray(int[] arr) {
+        for(int i=0; i<arr.length; ++i){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort2(n, arr);
+
+        scanner.close();
+    }
+}
