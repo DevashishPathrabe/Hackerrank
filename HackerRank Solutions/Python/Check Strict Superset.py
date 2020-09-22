18011d7b7e3a4e5a04f4ecb34be9c9a4942df0f0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-23 02:47:33 +0530

    Delete Check Strict Superset.py

--- HackerRank Solutions/Python/Check Strict Superset.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-A = set(map(int, input().split()))
-for i in range(int(input())):
-    X = set(map(int, input().split()))
-    if(A.issuperset(X) != True or len(A) == len(X)): 
-        print(False)
-        break 
-else: 
-    print(True)
