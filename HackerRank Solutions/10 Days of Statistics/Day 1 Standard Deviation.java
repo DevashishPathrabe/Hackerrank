b5df2f2c87aaf2277b7ffa6be640d2e94cf4406e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-23 20:26:48 +0530

    Day 1: Standard Deviation
    Here is my Day 1: Standard Deviation HackerRank Solution...

---
+++ HackerRank Solutions/10 Days of Statistics/Day 1 Standard Deviation.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int [] arr = new int[n];
+        double sum = 0;
+        for(int i=0; i<n; i++){
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+        }
+        double mean = sum / n;
+        double totalSum = 0;
+        for(int i=0; i<n; i++){
+            totalSum += Math.pow(arr[i] - mean, 2);
+        }
+        System.out.format("%.1f", Math.sqrt(totalSum / n));
+    }
+}
