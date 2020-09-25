85a6111894a9c9eda38fc4d2641514f21296c4bc
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-09-25 12:24:30 +0530

    Day 3: Intro to Conditional Statements
    Here is my Day 3: Intro to Conditional Statements HackerRank Solution...

---
+++ HackerRank Solutions/30 Days of Code/Day 3 Intro to Conditional Statements.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+    if(N%2 == 1):
+        print("Weird")
+    if(N%2 == 0 and 2 <= N and N <= 5):
+        print("Not Weird")
+    if(N%2 == 0 and 6 <= N and N <= 20):
+        print("Weird")
+    if(N%2 == 0 and N > 20):
+        print("Not Weird")
