363eeb47fd936629fa3643c5bcba49a463341917
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-10-30 19:40:12 +0530

    Day 10: Binary Numbers
    Here is my Day 10: Binary Numbers HackerRank Solution...

---
+++ HackerRank Solutions/30 Days of Code/Day 10 Binary Numbers.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+
+
+int main()
+{
+    int n, count=0, max=0;;
+    scanf("%d", &n);
+    while(n){
+        if(n%2 == 1){
+            count++;
+        } else{
+            count = 0;
+        }
+        if(max < count){
+            max = count;
+        }
+        n >>= 1;
+    }
+    printf("%d", max);
+    return 0;
+}
