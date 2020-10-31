075cfde029182dcbc2fa0102a9141cd788830524
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-10-31 08:53:10 +0530

    Day 11: 2D Arrays
    Here is my Day 11: 2D Arrays HackerRank Solution...

---
+++ HackerRank Solutions/30 Days of Code/Day 11 2D Arrays.c
@@ -0,0 +1,98 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char** split_string(char*);
+
+
+
+int main()
+{
+    int** arr = malloc(6 * sizeof(int*));
+
+    for (int i = 0; i < 6; i++) {
+        *(arr + i) = malloc(6 * (sizeof(int)));
+
+        char** arr_item_temp = split_string(readline());
+
+        for (int j = 0; j < 6; j++) {
+            char* arr_item_endptr;
+            char* arr_item_str = *(arr_item_temp + j);
+            int arr_item = strtol(arr_item_str, &arr_item_endptr, 10);
+
+            if (arr_item_endptr == arr_item_str || *arr_item_endptr != '\0') { exit(EXIT_FAILURE); }
+
+            *(*(arr + i) + j) = arr_item;
+        }
+    }
+    int sum=0, maxSum=-99999;
+    for(int i=1; i<5; i++){
+        for(int j=1; j<5; j++){
+            sum = arr[i][j] + arr[i-1][j] + arr[i-1][j-1] + arr[i-1][j+1] + arr[i+1][j] + arr[i+1][j-1] + arr[i+1][j+1];
+            if(maxSum < sum){
+                maxSum = sum;
+            }
+        }
+    }
+    printf("%d", maxSum);
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
+
+char** split_string(char* str) {
+    char** splits = NULL;
+    char* token = strtok(str, " ");
+
+    int spaces = 0;
+
+    while (token) {
+        splits = realloc(splits, sizeof(char*) * ++spaces);
+        if (!splits) {
+            return splits;
+        }
+
+        splits[spaces - 1] = token;
+
+        token = strtok(NULL, " ");
+    }
+
+    return splits;
+}
