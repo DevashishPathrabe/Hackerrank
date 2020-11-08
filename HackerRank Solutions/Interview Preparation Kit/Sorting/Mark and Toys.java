f3d3d7efc3e374c77387348fca7782a359228538
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-08 20:43:59 +0530

    Mark and Toys
    Here is my Mark and Toys HackerRank Solution...

--- HackerRank Solutions/Interview Preparation Kit/Sorting/Mark and Toys.java
+++ HackerRank Solutions/Interview Preparation Kit/Sorting/Mark and Toys.java
@@ -9,25 +9,44 @@ import java.util.regex.*;
 public class Solution {
 
     // Complete the maximumToys function below.
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int k = input.nextInt();
-        int[] prices = new int[n];
-        for(int i=0; i<n; i++){
-            prices[i] = input.nextInt();
-        }
+    static int maximumToys(int[] prices, int k) {
         Arrays.sort(prices);
-        int maxNoOfToys = 0;
-        for(int i=0; i<n; i++){
+        int i=0;
+        while(k>0 && i<prices.length){
             k -= prices[i];
-            if(k > 0){
-                maxNoOfToys++;
-            }
-            if(k <= 0){
-                break;
-            }
+            i++;
         }
-        System.out.println(maxNoOfToys);
+        return i-1;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] prices = new int[n];
+
+        String[] pricesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int pricesItem = Integer.parseInt(pricesItems[i]);
+            prices[i] = pricesItem;
+        }
+
+        int result = maximumToys(prices, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
