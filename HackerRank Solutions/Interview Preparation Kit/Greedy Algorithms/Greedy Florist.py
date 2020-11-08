e8f47cd841a10e740239078aeeaae04a5be802c6
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-09 00:13:00 +0530

    Greedy Florist
    Here is my Greedy Florist HackerRank Solution...

---
+++ HackerRank Solutions/Interview Preparation Kit/Greedy Algorithms/Greedy Florist.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the getMinimumCost function below.
+def getMinimumCost(k, c):
+    c.sort(reverse=True)
+    minCost = 0
+    previousPurchase = 0
+    for i in range(n):
+        minCost += (previousPurchase +1) * c[i]
+        if((i+1)%k == 0):
+            previousPurchase += 1
+    return minCost
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    c = list(map(int, input().rstrip().split()))
+
+    minimumCost = getMinimumCost(k, c)
+
+    fptr.write(str(minimumCost) + '\n')
+
+    fptr.close()
