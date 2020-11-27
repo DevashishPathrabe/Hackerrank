ea12b14286eb2fcbba721a6e354e21f8d1b2adae
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-27 20:37:24 +0530

    Insert a node at a specific position in a linked list
    Here is my Insert a node at a specific position in a linked list HackerRank Solution...

---
+++ HackerRank Solutions/Interview Preparation Kit/Linked Lists/Insert a node at a specific position in a linked list.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the insertNodeAtPosition function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head, int data, int position) {
+        SinglyLinkedListNode current = head;
+        if(position == 0){
+            current = new SinglyLinkedListNode(data);
+            current.next = head;
+            return current;
+        } else{
+            while(--position > 0){
+                current = current.next;
+            }
+            SinglyLinkedListNode node = current.next;
+            current.next = new SinglyLinkedListNode(data);
+            current.next.next = node;
+            return head;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
