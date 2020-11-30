011f01912d7bda98c59c76805fccd50c27444eb9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-30 12:24:00 +0530

    Recursion: Fibonacci Numbers
    Here is my Recursion: Fibonacci Numbers HackerRank Solution...

---
+++ HackerRank Solutions/Interview Preparation Kit/Recursion and Bactracking/Recursion Fibonacci Numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+    // Complete the function.
+    if(n==0 || n==1){
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
