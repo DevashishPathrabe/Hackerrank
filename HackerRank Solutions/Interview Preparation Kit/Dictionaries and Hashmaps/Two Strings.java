7fab497226cbdf3da75c2fc3a4da5ccec1a4c928
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-11-30 15:02:13 +0530

    Two Strings
    Here is my Two Strings HackerRank Solution...

---
+++ HackerRank Solutions/Interview Preparation Kit/Dictionaries and Hashmaps/Two Strings.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the twoStrings function below.
+    static String twoStrings(String s1, String s2) {
+        int[] a = new int[256];
+        for(int i=0; i<s1.length(); i++){
+            a[(int)s1.charAt(i)]++;
+        }
+        for(int i=0; i<s2.length(); i++){
+            if(a[(int)s2.charAt(i)] != 0){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s1 = scanner.nextLine();
+
+            String s2 = scanner.nextLine();
+
+            String result = twoStrings(s1, s2);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
