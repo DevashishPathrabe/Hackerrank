8cb53ed44be6dfec8a05eafc365b2c0cadde39b4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2020-12-06 14:48:43 +0530

    Day 5: Template Literals
    Here is my Day 5: Template Literals HackerRank Solution...

---
+++ HackerRank Solutions/10 Days of Javascript/Day 5 Template Literals.js
@@ -0,0 +1,54 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Determine the original side lengths and return an array:
+ * - The first element is the length of the shorter side
+ * - The second element is the length of the longer side
+ * 
+ * Parameter(s):
+ * literals: The tagged template literal's array of strings.
+ * expressions: The tagged template literal's array of expression values (i.e., [area, perimeter]).
+ */
+function sides(literals, ...expressions) {
+    let result;
+    let s = Math.sqrt(Math.pow(expressions[1],2)-16 * expressions[0])
+    let s1 = (expressions[1] + s)/4;
+    let s2 = (expressions[1] - s)/4;
+    result = [s1, s2];
+    return result.sort();
+}
+
+
+function main() {
+    let s1 = +(readLine());
+    let s2 = +(readLine());
+    
+    [s1, s2] = [s1, s2].sort();
+    
+    const [x, y] = sides`The area is: ${s1 * s2}.\nThe perimeter is: ${2 * (s1 + s2)}.`;
+    
+    console.log((s1 === x) ? s1 : -1);
+    console.log((s2 === y) ? s2 : -1);
+}
