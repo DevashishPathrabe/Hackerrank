9318f21ead9cbc79e1c0ee2808c9f32c60b771fd
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-02-20 23:59:32 +0530

    Update Day 0 Mean, Median, and Mode.java

--- HackerRank Solutions/10 Days of Statistics/Day 0 Mean, Median, and Mode.java
+++ HackerRank Solutions/10 Days of Statistics/Day 0 Mean, Median, and Mode.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int size = scan.nextInt();
