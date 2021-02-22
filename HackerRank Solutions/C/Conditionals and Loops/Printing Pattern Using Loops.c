b512689f41ca331c26cb567baf3877c866f43771
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-02-22 23:22:48 +0530

    Printing Pattern Using Loops
    Here is my Printing Pattern Using Loops HackerRank Solution...

---
+++ HackerRank Solutions/C/Conditionals and Loops/Printing Pattern Using Loops.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int k,n;
+    scanf("%d",&n);
+    k=n;
+    for(int i=0; i<n+(n-1); i++){
+        for(int j=0; j<n+(n-1); j++){
+            if(i <= n-1){        
+                if(i == 0){
+                    printf("%d ", k);
+                }
+                if(i >= 1){
+                    if(j < i){
+                        printf("%d ", k-j);
+                    }
+                    else if(j>=i && j<n+(n-1)-i){
+                        printf("%d ", k-i);
+                    }
+                    else{
+                        printf("%d ",(j-k+1)+1);
+                    }
+                }
+            }
+            else if(i == n-1){
+                if(j < n){
+                    printf("%d ", k-j);
+                }
+                else{
+                    printf("%d ", (j-k+1)+1);
+                }
+            }
+            else if(i >= n){
+                if(i==n+(n-1)){
+                    printf("%d ", k);
+                }
+                if(j < n+(n-1)-i-1){
+                    printf("%d ", k-j);
+                }
+                else if(j>=n+(n-1)-i-1 && j<(n+(n-1))-(n+(n-1)-i-1)){
+                    printf("%d ", k-(n+(n-1)-i-1));
+                }
+                else{
+                    printf("%d ", (j-k+1)+1);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
