2ef74a18077b6c0336f4b195d21a10b6c05b0988
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-02-27 00:41:50 +0530

    Candies
    Here is my Candies HackerRank Solution...

---
+++ HackerRank Solutions/Interview Preparation Kit/Dynamic Programming/Candies.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the candies function below.
+def candies(n, arr):
+    candies = [1]*n
+    for i in range(n-1):
+        if arr[i+1]>arr[i]:
+            candies[i+1] = candies[i]+1
+    for i in range(n-1,0,-1):
+        if arr[i-1]>arr[i] and candies[i-1]<=candies[i]:
+            candies[i-1] = candies[i]+1
+    return sum(candies)
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input())
+        arr.append(arr_item)
+
+    result = candies(n, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
