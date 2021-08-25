1143567d6f1f273c494ac1ea5df16ef2521020a9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-08-25 15:15:02 +0530

    Lower Bound-STL

--- HackerRank Solutions/C++/STL/Lower Bound-STL.cpp
+++ HackerRank Solutions/C++/STL/Lower Bound-STL.cpp
@@ -8,23 +8,25 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    vector<int> arr;
-    vector<int>::iterator it;
-    int N, Q, Y;
+    int N;
     cin >> N;
-    for(int i=0; i<N; i++){
+    vector<int> v;
+    vector<int>::iterator it;
+    for (int i=0; i<N; i++) {
         int temp;
         cin >> temp;
-        arr.push_back(temp);
+        v.push_back(temp);
     }
-    cin >> Q;
-    for(int i=0; i<Q; i++) {
+    int q;
+    cin >> q;
+    for (int i=0; i<q; i++) {
+        int Y;
         cin >> Y;
-        it = lower_bound(arr.begin(), arr.end(), Y);
+        it = lower_bound(v.begin(), v.end(), Y);
         if(*it == Y){
-            cout << "Yes " << it - arr.begin() + 1 << endl;
+            cout << "Yes " << it-v.begin()+1 << endl;
         } else {
-            cout << "No " << it - arr.begin() + 1 << endl;
+            cout << "No " << it-v.begin()+1 << endl;
         }
     }
     return 0;
