61b7359e16c876aa8d742b1cb5de62d4c0094282
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-08-26 15:53:09 +0530

    Basic Data Types

--- HackerRank Solutions/C++/Introduction/Basic Data Types.cpp
+++ HackerRank Solutions/C++/Introduction/Basic Data Types.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <cstdio>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
     // Complete the code.
     int a;
-    long int b;
+    long b;
     char c;
     float d;
     double e;
-    scanf("%d %ld %c %f %lf", &a, &b, &c, &d, &e);
-    printf("%d\n%ld\n%c\n%f\n%lf", a, b, c, d, e);
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
     return 0;
 }
