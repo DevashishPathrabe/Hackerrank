655859618f51c27cdc2331c236befb23eaea69b4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-08-30 01:00:15 +0530

    A Very Big Sum

--- HackerRank Solutions/Algorithms/Warmup/A Very Big Sum/A Very Big Sum.cpp
+++ HackerRank Solutions/Algorithms/Warmup/A Very Big Sum/A Very Big Sum.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
     int n;
-    long long int sum = 0;
+    long long sum = 0;
+    vector<long> v;
     cin >> n;
-    vector<int> arr(n);
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
-        sum += arr[i];
+    for (int i=0; i<n; i++){
+        long temp;
+        cin >> temp;
+        v.push_back(temp);
+        sum += v[i];
     }
     cout << sum << endl;
-    return 0;
 }
