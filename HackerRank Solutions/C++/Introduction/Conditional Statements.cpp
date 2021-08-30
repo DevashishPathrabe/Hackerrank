1842d5cc268a31497fcd4c887a7b507e2f59e4b6
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-08-31 01:07:51 +0530

    Conditional Statements

--- HackerRank Solutions/C++/Introduction/Conditional Statements.cpp
+++ HackerRank Solutions/C++/Introduction/Conditional Statements.cpp
@@ -4,27 +4,35 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    if(n == 1){
-        printf("one");
-    } else if(n == 2){
-        printf("two");
-    } else if(n == 3){
-        printf("three");
-    } else if(n == 4){
-        printf("four");
-    } else if(n == 5){
-        printf("five");
-    } else if(n == 6){
-        printf("six");
-    } else if(n == 7){
-        printf("seven");
-    } else if (n == 8) {
-        printf("eight");
-    } else if (n == 9) {
-        printf("nine");
-    } else {
-        printf("Greater than 9");
+    if (n == 1){
+        cout << "one" << endl;
+    }
+    else if (n == 2){
+        cout << "two" << endl;
+    }
+    else if (n == 3){
+        cout << "three" << endl;
+    }
+    else if (n == 4){
+        cout << "four" << endl;
+    }
+    else if (n == 5){
+        cout << "five" << endl;
+    }
+    else if (n == 6){
+        cout << "six" << endl;
+    }
+    else if (n == 7){
+        cout << "seven" << endl;
+    }
+    else if (n == 8){
+        cout << "eight" << endl;
+    }
+    else if (n == 9){
+        cout << "nine" << endl;
+    }
+    else{
+        cout << "Greater than 9" << endl;
     }
     return 0;
 }
