8c064ee251763be33983698dfe5503d007db361a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-09-01 00:55:41 +0530

    For Loop

--- HackerRank Solutions/C++/Introduction/For Loop.cpp
+++ HackerRank Solutions/C++/Introduction/For Loop.cpp
@@ -5,31 +5,42 @@ using namespace std;
 int main() {
     // Complete the code.
     int a, b;
-    scanf("%d\n%d", &a, &b);
-    for(int n = a; n <= b; n++){
-        if (n == 1){
-            printf("one\n");
-        }else if (n == 2){
-            printf("two\n");
-        }else if (n == 3){
-            printf("three\n");
-        }else if (n == 4){
-            printf("four\n");
-        }else if (n == 5){
-            printf("five\n");
-        }else if (n == 6){
-            printf("six\n");
-        }else if (n == 7){
-            printf("seven\n");
-        }else if (n == 8){
-            printf("eight\n");
-        }else if (n == 9){
-            printf("nine\n");
-        }else {
-            if (n % 2 == 0){
-                printf("even\n");
-            }else{
-                printf("odd\n");
+    cin >> a;
+    cin >> b;
+    for (int i=a; i<=b; i++){
+        if (i == 1){
+            cout << "one" << endl;
+        }
+        else if (i == 2){
+            cout << "two" << endl;
+        }
+        else if (i == 3){
+            cout << "three" << endl;
+        }
+        else if (i == 4){
+            cout << "four" << endl;
+        }
+        else if (i == 5){
+            cout << "five" << endl;
+        }
+        else if (i == 6){
+            cout << "six" << endl;
+        }
+        else if (i == 7){
+            cout << "seven" << endl;
+        }
+        else if (i == 8){
+            cout << "eight" << endl;
+        }
+        else if (i == 9){
+            cout << "nine" << endl;
+        }
+        else{
+            if (i % 2 == 0){
+                cout << "even" << endl;
+            }
+            else{
+                cout << "odd" << endl;
             }
         }
     }
