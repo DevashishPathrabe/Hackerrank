c285e4b334ec47f3e3593c3d8db165bdc8d10b98
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2021-09-03 21:25:17 +0530

    Variable Sized Arrays

--- HackerRank Solutions/C++/Introduction/Variable Sized Arrays.cpp
+++ HackerRank Solutions/C++/Introduction/Variable Sized Arrays.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -10,22 +11,22 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n, q;
     cin >> n >> q;
-    vector<int> arr[n];
-    for(int i=0; i<n; i++){
+    vector<vector<int>> v;
+    for (int i=0; i<n; i++){
         int k;
         cin >> k;
-        vector<int> tempVector;
-        for(int j=0; j<k; j++){
-            int tempVar;
-            cin >> tempVar;
-            tempVector.push_back(tempVar);
+        vector<int> tempVec;
+        for (int j=0; j<k; j++){
+            int temp;
+            cin >> temp;
+            tempVec.push_back(temp);
         }
-        arr[i] = tempVector;
+        v.push_back(tempVec);
     }
-    for(int i=0; i<q; i++){
+    for (int i=0; i<q; i++){
         int a, b;
         cin >> a >> b;
-        cout << arr[a][b] << endl;
+        cout << v[a][b] << endl;
     }
     return 0;
 }
