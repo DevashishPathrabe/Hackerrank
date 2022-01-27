989c3517df9387a2c3410145484c22286be36633
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:45:46 +0530

    Library Fine

--- HackerRank Solutions/Algorithms/Implementation/Library Fine/Library Fine.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Library Fine/Library Fine.cpp
@@ -7,16 +7,16 @@ vector<string> split_string(string);
 // Complete the libraryFine function below.
 int libraryFine(int d1, int m1, int y1, int d2, int m2, int y2) {
     int fine = 0;
-    if(y1>y2){
+    if (y1 > y2){
         fine = 10000;
     }
-    else if(y1<=y2){
-        if(m1>m2 && y1>=y2){
-            fine = 500 * (m1-m2);
+    else if (y1 <= y2){
+        if (m1 > m2 && y1 >= y2){
+            fine = 500 * (m1 - m2);
         }
-        else if(m1<=m2){
-            if(d1>d2 && (m1-m2)>=0 && y1>=y2){
-                fine = 15*(d1-d2);
+        else if (m1 <= m2){
+            if (d1 > d2 && (m1 - m2) >= 0 && y1 >= y2){
+                fine = 15*(d1 - d2);
             }
             else{
                 fine = 0;
