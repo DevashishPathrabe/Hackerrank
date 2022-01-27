d7ae86fe7ca9e13c18db3c5fb3d627ac25572ef3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:43:56 +0530

    Extra Long Factorials

--- HackerRank Solutions/Algorithms/Implementation/Extra Long Factorials/Extra Long Factorials.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Extra Long Factorials/Extra Long Factorials.cpp
@@ -6,20 +6,20 @@ int main(){
     cin >> n;
     a[0] = 1;
     size = 1;
-    for(i=2; i<=n; i++){
-        for(j=0; j<size; j++){
+    for (i=2; i<=n; i++){
+        for (j=0; j<size; j++){
             t = a[j]*i+c;
             c = t/10;
             a[j] = t%10;
         }
-        while(c){
+        while (c){
             a[j] = c%10;
             size++;
             j++;
             c = c/10;
         }
     }
-    for(i=size-1; i>=0; i--){
+    for (i=size-1; i>=0; i--){
         cout << a[i];
     }
     return 0; 
