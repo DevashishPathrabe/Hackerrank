40e92a38cc3a50f9f451118638f382d994d57223
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:59:24 +0530

    Apple and Orange

--- HackerRank Solutions/Algorithms/Implementation/Apple and Orange/Apple and Orange.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Apple and Orange/Apple and Orange.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
     int s, t, a, b, m, n, apple, totalapples = 0, orange, totaloranges = 0;
     cin >> s >> t >> a >> b >> m >> n;
-    for(int i=0; i<m; i++){
+    for (int i=0; i<m; i++){
         cin >> apple;
-        if(a+apple >= s && a+apple <= t){
+        if (a+apple >= s && a+apple <= t){
             totalapples++;
         }
     }
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         cin >> orange;
-        if(b+orange >= s && b+orange <= t){
+        if (b+orange >= s && b+orange <= t){
             totaloranges++;
         }
     }
