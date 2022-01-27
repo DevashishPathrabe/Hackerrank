6732b3a79af01851f291a6b3fbc3a39a92a3bb7a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:22:54 +0530

    Digit Frequency

--- HackerRank Solutions/C/Arrays and Strings/Digit Frequency.c
+++ HackerRank Solutions/C/Arrays and Strings/Digit Frequency.c
@@ -12,15 +12,15 @@ int main() {
     s = realloc(s, strlen(s) + 1);
     int len = strlen(s), i;
     int arr[10];
-    for(i=0; i<10; i++){
+    for (i=0; i<10; i++){
         arr[i] = 0;
     }
-    for(i=0; i<len; i++){
+    for (i=0; i<len; i++){
         if(s[i] >= '0' && s[i] <= '9'){
             arr[(int)(s[i] - '0')]++;
         }
     }
-    for(i=0; i<10; i++){
+    for (i=0; i<10; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
