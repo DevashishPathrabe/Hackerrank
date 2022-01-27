7ec3a03253bd5489f67e70403cb5e22d98669671
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:45:14 +0530

    Classes and Objects

--- HackerRank Solutions/C++/Classes/Classes and Objects.cpp
+++ HackerRank Solutions/C++/Classes/Classes and Objects.cpp
@@ -15,15 +15,15 @@ class Student
     public:
         void input()
         {
-            for(int i=0;i<5;i++)
+            for (int i=0; i<5; i++)
                 std::cin>>scores[i];
 
         }
         int calculateTotalScore()
         {
             int sum=0;
-            for(int i=0;i<5;i++)
-                sum=sum+scores[i];
+            for (int i=0; i<5; i++)
+                sum = sum + scores[i];
             return sum;
         }       
 };
