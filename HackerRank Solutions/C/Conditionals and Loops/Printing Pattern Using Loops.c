4547a67fd53f24f39f0e30fc93075792212b4987
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:34:31 +0530

    Printing Pattern Using Loops

--- HackerRank Solutions/C/Conditionals and Loops/Printing Pattern Using Loops.c
+++ HackerRank Solutions/C/Conditionals and Loops/Printing Pattern Using Loops.c
@@ -4,20 +4,20 @@
 #include <stdlib.h>
 
 int main() {
-    int k,n;
-    scanf("%d",&n);
-    k=n;
-    for(int i=0; i<n+(n-1); i++){
-        for(int j=0; j<n+(n-1); j++){
-            if(i <= n-1){        
-                if(i == 0){
+    int k, n;
+    scanf("%d", &n);
+    k = n;
+    for (int i=0; i<n+(n-1); i++){
+        for (int j=0; j<n+(n-1); j++){
+            if (i <= n-1){        
+                if (i == 0){
                     printf("%d ", k);
                 }
-                if(i >= 1){
-                    if(j < i){
+                if (i >= 1){
+                    if (j < i){
                         printf("%d ", k-j);
                     }
-                    else if(j>=i && j<n+(n-1)-i){
+                    else if (j>=i && j<n+(n-1)-i){
                         printf("%d ", k-i);
                     }
                     else{
@@ -25,22 +25,22 @@ int main() {
                     }
                 }
             }
-            else if(i == n-1){
-                if(j < n){
+            else if (i == n-1){
+                if (j < n){
                     printf("%d ", k-j);
                 }
                 else{
                     printf("%d ", (j-k+1)+1);
                 }
             }
-            else if(i >= n){
-                if(i==n+(n-1)){
+            else if (i >= n){
+                if (i == n+(n-1)){
                     printf("%d ", k);
                 }
-                if(j < n+(n-1)-i-1){
+                if (j < n+(n-1)-i-1){
                     printf("%d ", k-j);
                 }
-                else if(j>=n+(n-1)-i-1 && j<(n+(n-1))-(n+(n-1)-i-1)){
+                else if (j>=n+(n-1)-i-1 && j<(n+(n-1))-(n+(n-1)-i-1)){
                     printf("%d ", k-(n+(n-1)-i-1));
                 }
                 else{
