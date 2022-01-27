598192f956041f55e9b6943ad5da17f45000add0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:42:48 +0530

    Boxes through a Tunnel

--- HackerRank Solutions/C/Structs and Enums/Boxes through a Tunnel.c
+++ HackerRank Solutions/C/Structs and Enums/Boxes through a Tunnel.c
@@ -11,9 +11,9 @@ int get_volume(box b){
 	return b.length * b.width * b.height;
 }
 int is_lower_than_max_height(box b) {
-	if(b.height < 41){
+	if (b.height < 41){
         return 1; 
-    }else{
+    } else{
         return 0; 
     }
 }
