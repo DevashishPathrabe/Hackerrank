e3b5fc5d624e4801a7ba64eec0d4d5a34c01ca5a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:59:04 +0530

    Angry Professor

--- HackerRank Solutions/Algorithms/Implementation/Angry Professor/Angry Professor.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Angry Professor/Angry Professor.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main(){
     int t, StudentsArrivalTime;
     cin >> t;
-    for(int a0=0; a0<t; a0++){
+    for (int a0=0; a0<t; a0++){
         int n, k;
         cin >> n >> k;
         vector<int>a(n);
         StudentsArrivalTime = 0;
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             cin >> a[i];
-            if(a[i] <= 0){
+            if (a[i] <= 0){
                 StudentsArrivalTime++;
             }
         }
-        if(StudentsArrivalTime >= k){
+        if (StudentsArrivalTime >= k){
             cout << "NO" << endl;
         } else{
             cout << "YES" << endl;
