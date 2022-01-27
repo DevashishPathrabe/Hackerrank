21e5511fd164350dee72613bda8c6c98f1ae4d7d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:45:24 +0530

    Exceptional Server

--- HackerRank Solutions/C++/Classes/Exceptional Server.cpp
+++ HackerRank Solutions/C++/Classes/Exceptional Server.cpp
@@ -12,12 +12,12 @@ private:
 public:
 	static int compute(long long A, long long B) {
 		load += 1;
-		if(A < 0) {
+		if (A < 0) {
 			throw std::invalid_argument("A is negative");
 		}
 		vector<int> v(A, 0);
 		int real = -1, cmplx = sqrt(-1);
-		if(B == 0) throw 0;
+		if (B == 0) throw 0;
 		real = (A/B)*real;
 		int ans = v.at(B);
 		return real + A - B*ans;
