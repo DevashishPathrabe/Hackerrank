330255bb7c319c7f60d92e258ede23ad61c0d46d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:39:11 +0530

    Students Marks Sum

--- HackerRank Solutions/C/Functions/Students Marks Sum.c
+++ HackerRank Solutions/C/Functions/Students Marks Sum.c
@@ -7,13 +7,13 @@
 
 int marks_summation(int* marks, int number_of_students, char gender) {
     int sum = 0;
-    if(gender == 'b'){
-        for(int i = 0; i < number_of_students; i += 2){
+    if (gender == 'b'){
+        for (int i = 0; i < number_of_students; i += 2){
             sum += marks[i];
         }
     }
-    else if(gender == 'g'){
-        for(int i = 1;i < number_of_students;i += 2){
+    else if (gender == 'g'){
+        for (int i = 1;i < number_of_students;i += 2){
             sum += marks[i];
         }
     }
