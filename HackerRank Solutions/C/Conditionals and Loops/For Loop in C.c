6384a26ce39db0a2d19cf73c0b35bd67fc90c8c8
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:29:19 +0530

    For Loop in C

--- HackerRank Solutions/C/Conditionals and Loops/For Loop in C.c
+++ HackerRank Solutions/C/Conditionals and Loops/For Loop in C.c
@@ -10,11 +10,11 @@ int main()
     int a, b;
     scanf("%d\n%d", &a, &b);
     char str[][100]={"zero","one","two","three","four","five","six","seven","eight","nine"};
-    for(int i=a;i<=b;i++){
-        if(i<=9){
+    for (int i=a; i<=b; i++){
+        if (i <= 9){
             printf("%s\n",str[i]);
         }else{
-            if(i%2==0){
+            if (i%2 == 0){
                 printf("even\n");
             }else{
                 printf("odd\n");
