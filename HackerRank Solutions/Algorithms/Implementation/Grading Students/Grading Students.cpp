a088283384cc723a5bbe8440a4b5500ee8ec3f1b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:44:48 +0530

    Grading Students

--- HackerRank Solutions/Algorithms/Implementation/Grading Students/Grading Students.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Grading Students/Grading Students.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for(int i=0;i<n;i++){
+    for (int i=0; i<n; i++){
         int grade;
         cin >> grade;
         int nextMultiple=grade;
-        if(grade < 38){
+        if (grade < 38){
             cout << grade << endl;
             continue;
         }
-        while(nextMultiple % 5){
+        while (nextMultiple % 5){
             nextMultiple++;
         }
-        if(nextMultiple - grade < 3){
+        if (nextMultiple - grade < 3){
             cout << nextMultiple << endl;
         } else{
             cout << grade << endl;
