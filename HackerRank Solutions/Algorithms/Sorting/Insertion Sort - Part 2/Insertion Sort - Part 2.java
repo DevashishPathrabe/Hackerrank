b7c259b7d6be4663b9bde0920aef042e7e2f3089
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:54:09 +0530

    Insertion Sort - Part 2

--- HackerRank Solutions/Algorithms/Sorting/Insertion Sort - Part 2/Insertion Sort - Part 2.java
+++ HackerRank Solutions/Algorithms/Sorting/Insertion Sort - Part 2/Insertion Sort - Part 2.java
@@ -10,10 +10,10 @@ public class Solution {
 
     // Complete the insertionSort2 function below.
     static void insertionSort2(int n, int[] arr) {
-        for(int i=0; i<n-1; i++){
+        for (int i=0; i<n-1; i++){
             int j = i+1;
-            while(j > 0){
-                if(arr[j] < arr[j-1]){
+            while (j > 0){
+                if (arr[j] < arr[j-1]){
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
