6a6b720547dd531bdbe2e4750a29922f30e1df96
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 15:07:01 +0530

    Circular Array Rotation

--- HackerRank Solutions/Algorithms/Implementation/Circular Array Rotation/Circular Array Rotation.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Circular Array Rotation/Circular Array Rotation.cpp
@@ -5,14 +5,14 @@ int main() {
     int n, k, q, a, m;
     vector <int> arr;
     cin >> n >> k >> q;
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         cin >> a;
         arr.push_back(a);
     }
     k %= n;
-    for(int i=0; i<q; i++){
+    for (int i=0; i<q; i++){
         cin >> m;
-        if(m-k >= 0){
+        if (m-k >= 0){
             cout << arr[m-k] << endl;
         } else{
             cout << arr[m-k+n] << endl;
