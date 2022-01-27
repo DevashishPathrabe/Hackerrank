a03c77e9a7717e0e8c2516a07a6a9feaf22bfa33
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:47:50 +0530

    The Hurdle Race

--- HackerRank Solutions/Algorithms/Implementation/The Hurdle Race/The Hurdle Race.cpp
+++ HackerRank Solutions/Algorithms/Implementation/The Hurdle Race/The Hurdle Race.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int n, k, arr, h = 0;
     cin >> n >> k;
-    for (int i=0; i<n; i++) {
+    for (int i=0; i<n; i++){
         cin >> arr;
         if (arr > h){
             h = arr;
