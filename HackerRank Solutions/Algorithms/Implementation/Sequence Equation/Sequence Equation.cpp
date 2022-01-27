2c98a6f2f657bf5f7d8a945041092894c099c873
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:47:02 +0530

    Sequence Equation

--- HackerRank Solutions/Algorithms/Implementation/Sequence Equation/Sequence Equation.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Sequence Equation/Sequence Equation.cpp
@@ -5,12 +5,12 @@ int main() {
     int n;
     cin >> n;
     int p[n + 1];
-    for(int i=1; i<=n; i++){
+    for (int i=1; i<=n; i++){
         int k;
         cin >> k;
         p[k] = i;
     }
-    for(int i=1; i<=n; i++){
+    for (int i=1; i<=n; i++){
         cout << p[p[i]] << endl;
     }
     return 0;
