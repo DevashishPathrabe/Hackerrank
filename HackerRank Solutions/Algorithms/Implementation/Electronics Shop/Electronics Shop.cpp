6f26d1490314f97434bff6d257ce38e28bae3368
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:43:45 +0530

    Electronics Shop

--- HackerRank Solutions/Algorithms/Implementation/Electronics Shop/Electronics Shop.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Electronics Shop/Electronics Shop.cpp
@@ -5,14 +5,14 @@ int main()
 {
     int b, n, m, keyboard[1000], usb[1000], amount = -1;
     cin >> b >> n >> m;
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         cin >> keyboard[i];
     }
-    for(int j=0; j<m; j++){
+    for (int j=0; j<m; j++){
         cin >> usb[j];
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
             if (keyboard[i] + usb[j] <= b){
                 amount = max(amount, keyboard[i] + usb[j]);
             }
