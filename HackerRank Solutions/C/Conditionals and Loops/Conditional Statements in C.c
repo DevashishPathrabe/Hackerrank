ccc9f426e243aea12c003edc057349ef95355285
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:28:59 +0530

    Conditional Statements in C

--- HackerRank Solutions/C/Conditionals and Loops/Conditional Statements in C.c
+++ HackerRank Solutions/C/Conditionals and Loops/Conditional Statements in C.c
@@ -12,31 +12,31 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    if(n == 1) {
+    if (n == 1) {
         printf("one");
     }
-    else if(n == 2) {
+    else if (n == 2) {
         printf("two");
     }
-    else if(n == 3) {
+    else if (n == 3) {
         printf("three");
     }
-    else if(n == 4) {
+    else if (n == 4) {
         printf("four");
     }
-    else if(n == 5) {
+    else if (n == 5) {
         printf("five");
     }
-    else if(n == 6) {
+    else if (n == 6) {
         printf("six");
     }
-    else if(n == 7) {
+    else if (n == 7) {
         printf("seven");
     }
-    else if(n == 8) {
+    else if (n == 8) {
         printf("eight");
     }
-    else if(n == 9) {
+    else if (n == 9) {
         printf("nine");
     }
     else {
