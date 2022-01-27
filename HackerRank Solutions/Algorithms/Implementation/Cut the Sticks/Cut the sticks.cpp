09ba8c594162e3a42a1b6a07092f2282fcbafba7
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 15:07:19 +0530

    Cut the sticks

--- HackerRank Solutions/Algorithms/Implementation/Cut the Sticks/Cut the sticks.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Cut the Sticks/Cut the sticks.cpp
@@ -5,29 +5,29 @@ int main(){
     int n;
     cin >> n;
     int sticks[n], minLength = 10000;
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         cin >> sticks[i];
-        if(minLength > sticks[i]){
+        if (minLength > sticks[i]){
             minLength = sticks[i];
         }
     }
     bool continueLoop = true;
     int validSticks = n;
-    while(continueLoop){
+    while (continueLoop){
         cout << validSticks << endl;
         int newMinLength = 10000;
         validSticks = 0;
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             sticks[i] -= minLength;
-            if(sticks[i] > 0){
-                if(newMinLength > sticks[i]){
+            if (sticks[i] > 0){
+                if (newMinLength > sticks[i]){
                     newMinLength = sticks[i];
                 }
                 validSticks++;
             }
         }
         minLength = newMinLength;
-        if(validSticks == 0){
+        if (validSticks == 0){
             continueLoop = false;
         }
     }
