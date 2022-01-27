f146a8f6ebcd6609c455192e5cb8e4ae4c4e7e9a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 15:04:59 +0530

    Cavity Map

--- HackerRank Solutions/Algorithms/Implementation/Cavity Map/Cavity Map.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Cavity Map/Cavity Map.cpp
@@ -8,20 +8,20 @@ int main(){
     int n;
     char input[101][101];
     cin >> n;
-    for(int i=0; i<n; i++){
+    for (int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin >> input[i][j];
         }
     }
-    for(int i=1; i<n-1; i++){
-        for(int j=1; j<n-1; j++){
-            if(CONDITION_1 && CONDITION_2){
+    for (int i=1; i<n-1; i++){
+        for (int j=1; j<n-1; j++){
+            if (CONDITION_1 && CONDITION_2){
                 input[i][j] = 'X';
             }
         }
     }
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
+    for (int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
             cout << input[i][j];
         }
         cout << endl;
