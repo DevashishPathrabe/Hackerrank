0757138a133d858d02663622b53a348b63e7714c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:46:24 +0530

    Migratory Birds

--- HackerRank Solutions/Algorithms/Implementation/Migratory Birds/Migratory Birds.java
+++ HackerRank Solutions/Algorithms/Implementation/Migratory Birds/Migratory Birds.java
@@ -11,11 +11,11 @@ public class Solution {
         int arr_count[] = new int[6];
         int mostNoOfCommonBird = 1; 
         int max = 0;
-        for(int i=0;i<n;i++){
+        for (int i=0; i<n; i++){
             arr_count[arr[i]]++;
         }
-        for(int i=1; i<=5;i++){
-            if(arr_count[i] > max){
+        for (int i=1; i<=5; i++){
+            if (arr_count[i] > max){
                 mostNoOfCommonBird = i;
                 max = arr_count[i];
             }
