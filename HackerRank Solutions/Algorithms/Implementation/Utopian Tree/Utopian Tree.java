fd4d6c40bfc16eee0528d53802e75f1e5e14021b
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:48:04 +0530

    Utopian Tree

--- HackerRank Solutions/Algorithms/Implementation/Utopian Tree/Utopian Tree.java
+++ HackerRank Solutions/Algorithms/Implementation/Utopian Tree/Utopian Tree.java
@@ -11,8 +11,8 @@ public class Solution {
     // Complete the utopianTree function below.
     static int utopianTree(int n) {
         int height = 1;       
-        for(int i=1; i<=n; i++){
-            if(i%2 != 0){
+        for (int i=1; i<=n; i++){
+            if (i%2 != 0){
                 height *= 2;
             } else{
                 height +=1;
