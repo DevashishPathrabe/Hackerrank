35e6df993eac50461fac23b258246a3de21b4c56
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:23:03 +0530

    Printing Tokens

--- HackerRank Solutions/C/Arrays and Strings/Printing Tokens.c
+++ HackerRank Solutions/C/Arrays and Strings/Printing Tokens.c
@@ -9,8 +9,8 @@ int main()
     s = malloc(1024 * sizeof(char));
     scanf("%[^\n]", s);
     s = realloc(s, strlen(s) + 1);
-    for(int i=0; i<strlen(s); i++) {
-        if(s[i] == ' '){
+    for (int i=0; i<strlen(s); i++) {
+        if (s[i] == ' '){
             printf("\n");
         } else{
             printf("%c", s[i]);
