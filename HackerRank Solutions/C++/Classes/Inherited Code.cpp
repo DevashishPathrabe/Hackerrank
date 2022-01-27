6c904f0b935deb216ed645a6520988bb318c29d4
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:45:33 +0530

    Inherited Code

--- HackerRank Solutions/C++/Classes/Inherited Code.cpp
+++ HackerRank Solutions/C++/Classes/Inherited Code.cpp
@@ -19,11 +19,11 @@ class BadLengthException {
 bool checkUsername(string username) {
 	bool isValid = true;
 	int n = username.length();
-	if(n < 5) {
+	if (n < 5) {
 		throw BadLengthException(n);
 	}
-	for(int i = 0; i < n-1; i++) {
-		if(username[i] == 'w' && username[i+1] == 'w') {
+	for (int i = 0; i < n-1; i++) {
+		if (username[i] == 'w' && username[i+1] == 'w') {
 			isValid = false;
 		}
 	}
