beb94422b6044824035e0a241c2b14c1b62363c0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:46:13 +0530

    Migratory Birds

--- HackerRank Solutions/Algorithms/Implementation/Migratory Birds/Migratory Birds.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Migratory Birds/Migratory Birds.cpp
@@ -8,13 +8,13 @@ int main()
         int arr[n];
         int occurrence;
         int count[6] = {0};
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             cin >> arr[i];
             count[arr[i]]++;
         }
         int max = count[0];
-        for(int i=0; i<=5; i++){
-            if(count[i] > max){
+        for (int i=0; i<=5; i++){
+            if (count[i] > max){
                 occurrence = i;
                 max = count[i];
             }
