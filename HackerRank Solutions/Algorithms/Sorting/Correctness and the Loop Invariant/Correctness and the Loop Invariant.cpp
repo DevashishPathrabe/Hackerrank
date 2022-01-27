657887e3c76b356ed8dbacdaf6f554f128e17e32
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:53:39 +0530

    Correctness and the Loop Invariant

--- HackerRank Solutions/Algorithms/Sorting/Correctness and the Loop Invariant/Correctness and the Loop Invariant.cpp
+++ HackerRank Solutions/Algorithms/Sorting/Correctness and the Loop Invariant/Correctness and the Loop Invariant.cpp
@@ -7,20 +7,17 @@
 void insertionSort(int N, int arr[]) {
     int i,j;
     int value;
-    for(i=1;i<N;i++)
-    {
-        value=arr[i];
-        j=i-1;
-        while(j>=0 && value<arr[j])
-        {
-            arr[j+1]=arr[j];
-            j=j-1;
+    for (i=1; i<N; i++){
+        value = arr[i];
+        j = i-1;
+        while (j >= 0 && value < arr[j]){
+            arr[j+1] = arr[j];
+            j = j-1;
         }
-        arr[j+1]=value;
+        arr[j+1] = value;
     }
-    for(j=0;j<N;j++)
-    {
-        printf("%d",arr[j]);
+    for (j=0; j<N; j++){
+        printf("%d", arr[j]);
         printf(" ");
     }
 }
