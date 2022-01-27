dccbfc5490e7756c4d129ca94b9e6d36c7ed40f0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:24:34 +0530

    Bitwise Operator

--- HackerRank Solutions/C/Conditionals and Loops/Bitwise Operator.c
+++ HackerRank Solutions/C/Conditionals and Loops/Bitwise Operator.c
@@ -9,19 +9,19 @@ void calculate_the_maximum(int n, int k) {
     int and, and_max = 0;
     int or, or_max = 0;
     int xor, xor_max = 0;
-    for(i = 1; i <= n; i++){
-        for(j = 1; j <= n; j++){
-            if(i < j){
+    for (i = 1; i <= n; i++){
+        for (j = 1; j <= n; j++){
+            if (i < j){
                 and = i & j;
-                if(and > and_max && and < k){
+                if (and > and_max && and < k){
                     and_max = and;
                 }
                 or = i | j;
-                if(or > or_max && or < k){
+                if (or > or_max && or < k){
                     or_max = or;
                 }
                 xor = i ^ j;
-                if(xor > xor_max && xor < k){
+                if (xor > xor_max && xor < k){
                     xor_max = xor;
                 }
             }
