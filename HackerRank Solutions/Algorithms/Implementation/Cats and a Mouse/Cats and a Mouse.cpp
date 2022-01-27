9b03cc6c9f7681bcee55833c88fd5c2fabd74ae1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 15:03:38 +0530

    Cats and a Mouse

--- HackerRank Solutions/Algorithms/Implementation/Cats and a Mouse/Cats and a Mouse.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Cats and a Mouse/Cats and a Mouse.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int q;
     cin >> q;
-    for (int i = 0; i < q; i++) {
+    for (int i=0; i<q; i++){
         int a, b, c;
         cin >> a >> b >> c;
         (abs(a - c) < abs(b - c)) ? cout << "Cat A" : abs(b - c) < abs(a - c) ? cout << "Cat B" : cout << "Mouse C";
