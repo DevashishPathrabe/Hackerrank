361a9de4603f9e1fbf5e19987d016ed5512f3e8f
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:39:00 +0530

    Calculate the Nth term

--- HackerRank Solutions/C/Functions/Calculate the Nth term.c
+++ HackerRank Solutions/C/Functions/Calculate the Nth term.c
@@ -6,14 +6,15 @@
 
 int find_nth_term(int n, int a, int b, int c) {
   //Write your code here.
-  if(n == 1){
+    if (n == 1){
         return a;
-    }else if(n == 2){
+    }
+    else if (n == 2){
         return b;
-    }else if(n == 3){
+    }else if (n == 3){
         return c;
     }
-    return find_nth_term(n-1,a,b,c)+find_nth_term(n-2,a,b,c)+find_nth_term(n-3,a,b,c);
+    return find_nth_term(n-1, a, b, c) + find_nth_term(n-2, a, b, c) + find_nth_term(n-3, a, b, c);
 }
 
 int main() {
