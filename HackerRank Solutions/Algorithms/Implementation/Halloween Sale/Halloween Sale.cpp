0809749b2e914585b0b299d3dfd3d81df6297d66
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 20:45:05 +0530

    Halloween Sale

--- HackerRank Solutions/Algorithms/Implementation/Halloween Sale/Halloween Sale.cpp
+++ HackerRank Solutions/Algorithms/Implementation/Halloween Sale/Halloween Sale.cpp
@@ -5,16 +5,16 @@ int main() {
     int p, d, m, s;
     int numberOfGames, t;
     cin >> p >> d >> m >> s;
-    if(s < p){
+    if (s < p){
         numberOfGames = 0;
     } else{
-        numberOfGames = 1 + (p - m)/d;
+        numberOfGames = 1 + (p - m) / d;
         t = numberOfGames * (2 * p - (numberOfGames-1) * d)/2;
-        if(s >= t){
-            numberOfGames += (s - t)/m;
+        if (s >= t){
+            numberOfGames += (s - t) / m;
         } else{
             int b = 2 * p + d;
-            numberOfGames = (b - sqrt(b*b-8*d*s))/(2 * d);
+            numberOfGames = (b - sqrt(b*b-8*d*s)) / (2 * d);
         }
     }
     cout << numberOfGames << endl;
