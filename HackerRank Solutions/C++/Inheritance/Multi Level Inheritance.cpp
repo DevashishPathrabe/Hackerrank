00f8b0ba3c29067f50e98303b290293836dccf35
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-27 14:53:29 +0530

    Multi Level Inheritance

--- HackerRank Solutions/C++/Inheritance/Multi Level Inheritance.cpp
+++ HackerRank Solutions/C++/Inheritance/Multi Level Inheritance.cpp
@@ -7,23 +7,23 @@ using namespace std;
 
 class Triangle{
 	public:
-		void triangle(){
-			cout<<"I am a triangle\n";
+		void triangle() {
+			cout << "I am a triangle\n";
 		}
 };
 
 class Isosceles : public Triangle{
   	public:
-  		void isosceles(){
-    		cout<<"I am an isosceles triangle\n";
+  		void isosceles() {
+    		cout << "I am an isosceles triangle\n";
   		}
 };
 
 //Write your code here.
 class Equilateral:public Isosceles{
     public:
-        void equilateral(){
-            cout<<"I am an equilateral triangle\n";
+        void equilateral() {
+            cout << "I am an equilateral triangle\n";
         }
 };
 
