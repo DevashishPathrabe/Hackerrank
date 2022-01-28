9242b245557bc99c5159f74db95528a5a648d9ff
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:20:06 +0530

    Day 4 Class vs. Instance

--- HackerRank Solutions/30 Days of Code/Day 4 Class vs. Instance/Day 4 Class vs. Instance.py
+++ HackerRank Solutions/30 Days of Code/Day 4 Class vs. Instance/Day 4 Class vs. Instance.py
@@ -1,16 +1,16 @@
 class Person:
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
-        if(initialAge > 0):
+        if (initialAge > 0):
             self.age = initialAge
         else:
             print("Age is not valid, setting age to 0.")
             self.age = 0
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
-        if(self.age < 13):
+        if (self.age < 13):
             print("You are young.")
-        elif(self.age < 18):
+        elif (self.age < 18):
             print("You are a teenager.")
         else:
             print("You are old.")
