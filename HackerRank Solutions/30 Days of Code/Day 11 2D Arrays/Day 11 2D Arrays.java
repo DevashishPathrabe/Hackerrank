b7205ff8b0d182bb7dfa0d9ce3353e6dc5afda55
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 19:51:27 +0530

    Day 11 2D Arrays

--- HackerRank Solutions/30 Days of Code/Day 11 2D Arrays/Day 11 2D Arrays.java
+++ HackerRank Solutions/30 Days of Code/Day 11 2D Arrays/Day 11 2D Arrays.java
@@ -15,20 +15,20 @@ public class Solution {
     public static void main(String[] args) {
         int[][] arr = new int[6][6];
 
-        for (int i = 0; i < 6; i++) {
+        for (int i=0; i<6; i++){
             String[] arrRowItems = scanner.nextLine().split(" ");
             scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            for (int j = 0; j < 6; j++) {
+            for (int j=0; j<6; j++){
                 int arrItem = Integer.parseInt(arrRowItems[j]);
                 arr[i][j] = arrItem;
             }
         }
         int sum=0, maxSum=-99999;
-        for(int i=1; i<5; i++){
-            for(int j=1; j<5; j++){
+        for (int i=1; i<5; i++){
+            for (int j=1; j<5; j++){
                 sum = arr[i][j] + arr[i-1][j] + arr[i-1][j-1] + arr[i-1][j+1] + arr[i+1][j] + arr[i+1][j-1] + arr[i+1][j+1];
-                if(maxSum < sum){
+                if (maxSum < sum){
                     maxSum = sum;
                 }
             }
