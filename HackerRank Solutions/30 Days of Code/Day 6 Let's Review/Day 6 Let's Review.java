6fef2a5a2927adc8062e7bfe4b80b57171a9aee9
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:27:34 +0530

    Day 6 Let's Review

---
+++ HackerRank Solutions/30 Days of Code/Day 6 Let's Review/Day 6 Let's Review.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt();
+        scan.nextLine();
+        for(int i=0; i<T; i++){
+            String string = scan.nextLine();
+            char[] charArray = string.toCharArray();
+            for(int j=0; j<charArray.length; j++){
+                if(j % 2 == 0){
+                    System.out.print(charArray[j]);
+                }
+            }
+            System.out.print(" ");
+            for(int j=0; j<charArray.length; j++){
+                if(j % 2 != 0){
+                    System.out.print(charArray[j]);
+                }
+            }
+            System.out.println();
+        }
+        scan.close();
+    }
+}
