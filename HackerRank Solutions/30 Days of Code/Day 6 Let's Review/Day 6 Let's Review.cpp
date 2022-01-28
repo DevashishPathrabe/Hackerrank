1a9b31d3a8f9e59c394c8a89338c4cfd7ec30a87
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:29:19 +0530

    Day 6 Let's Review

--- HackerRank Solutions/30 Days of Code/Day 6 Let's Review/Day 6 Let's Review.cpp
+++ HackerRank Solutions/30 Days of Code/Day 6 Let's Review/Day 6 Let's Review.cpp
@@ -10,17 +10,17 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int T;
     cin >> T;
-    for(int i=0; i<T; i++){
+    for (int i=0; i<T; i++){
         string str;
         cin >> str;
-        for(int j=0; j<str.length(); j++){
-            if(j % 2 == 0){
+        for (int j=0; j<str.length(); j++){
+            if (j % 2 == 0){
                 cout << str[j];
             }
         }
         cout << " ";
-        for(int j=0; j<str.length(); j++){
-            if(j % 2 != 0){
+        for (int j=0; j<str.length(); j++){
+            if (j % 2 != 0){
                 cout << str[j];
             }
         }
