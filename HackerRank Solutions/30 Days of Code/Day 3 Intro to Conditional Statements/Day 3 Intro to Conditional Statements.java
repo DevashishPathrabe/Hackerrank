312955a035e887ee8910ddb3968db370e274829e
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:16:23 +0530

    Day 3 Intro to Conditional Statements

--- HackerRank Solutions/30 Days of Code/Day 3 Intro to Conditional Statements/Day 3 Intro to Conditional Statements.java
+++ HackerRank Solutions/30 Days of Code/Day 3 Intro to Conditional Statements/Day 3 Intro to Conditional Statements.java
@@ -15,16 +15,16 @@ public class Solution {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        if(N%2 == 1){
+        if (N%2 == 1){
             System.out.println("Weird");
         }
-        if(N%2 == 0 && 2 <= N && N <= 5){
+        if (N%2 == 0 && 2 <= N && N <= 5){
             System.out.println("Not Weird");
         }
-        if(N%2 == 0 && 6 <= N && N <= 20){
+        if (N%2 == 0 && 6 <= N && N <= 20){
             System.out.println("Weird");
         }
-        if(N%2 == 0 && N > 20){
+        if (N%2 == 0 && N > 20){
             System.out.println("Not Weird");
         }
         scanner.close();
