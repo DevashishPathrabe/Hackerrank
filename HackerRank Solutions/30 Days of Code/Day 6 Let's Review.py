8ffcd8d6b7c1ab7f2d4655e50948af049961705c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:27:16 +0530

    Delete Day 6 Let's Review.py

--- HackerRank Solutions/30 Days of Code/Day 6 Let's Review.py
+++
@@ -1,6 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-T = int(input().strip())
-for i in range(0, T):
-    S = input()
-    even, odd = S[::2], S[1::2]
-    print(even + " " + odd)
