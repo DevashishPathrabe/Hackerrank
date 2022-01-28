2ca68c6add69f7cb7b1f33b384be74a8265f8f89
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:02:52 +0530

    Staircase

--- HackerRank Solutions/Algorithms/Warmup/Staircase/Staircase.java
+++ HackerRank Solutions/Algorithms/Warmup/Staircase/Staircase.java
@@ -10,9 +10,9 @@ public class Solution {
 
     // Complete the staircase function below.
     static void staircase(int n) {
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j<n-1-i){
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                if (j < n-1-i){
                     System.out.print(" ");
                 } else{
                     System.out.print("#");
