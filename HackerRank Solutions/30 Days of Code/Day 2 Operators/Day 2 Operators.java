5fdd36f78121cbd6bf38d1f685be5427365e23a2
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:11:34 +0530

    Day 2 Operators

--- HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.java
+++ HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.java
@@ -10,8 +10,8 @@ public class Solution {
 
     // Complete the solve function below.
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        double tip = meal_cost * tip_percent/100;
-        double tax = meal_cost * tax_percent/100;
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
         double totalCost = meal_cost + tip + tax;
         System.out.println(Math.round(totalCost));
     }
