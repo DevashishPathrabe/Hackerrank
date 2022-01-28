e9ed05b8be51d3e930396749dc3ab34b18a15eb1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:06:24 +0530

    Day 1 Data Types

---
+++ HackerRank Solutions/30 Days of Code/Day 1 Data Types/Day 1 Data Types.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+i1 = int(input())
+d1 = float(input())
+s1 = str(input())
+# Print the sum of both integer variables on a new line.
+print(i + i1)
+# Print the sum of the double variables on a new line.
+print(d + d1)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + s1)
