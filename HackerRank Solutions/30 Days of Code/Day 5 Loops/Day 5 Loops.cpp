5a2849c121503a88db16d8a5ef1a4ee713d376c0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:24:01 +0530

    Day 5 Loops

--- HackerRank Solutions/30 Days of Code/Day 5 Loops/Day 5 Loops.cpp
+++ HackerRank Solutions/30 Days of Code/Day 5 Loops/Day 5 Loops.cpp
@@ -9,7 +9,7 @@ int main()
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    for(int i=1; i<11; i++){
+    for (int i=1; i<11; i++){
         cout << n << " x " << i << " = " << n*i << endl;
     }
     return 0;
