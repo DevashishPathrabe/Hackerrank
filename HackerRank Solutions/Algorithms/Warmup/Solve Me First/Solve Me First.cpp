7b163951f11a8584ab76a1132621375e75e3e748
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:02:32 +0530

    Solve Me First

--- HackerRank Solutions/Algorithms/Warmup/Solve Me First/Solve Me First.cpp
+++ HackerRank Solutions/Algorithms/Warmup/Solve Me First/Solve Me First.cpp
@@ -12,9 +12,9 @@ int solveMeFirst(int a, int b) {
 int main() {
   int num1, num2;
   int sum;
-  cin>>num1>>num2;
-  sum = solveMeFirst(num1,num2);
-  cout<<sum;
+  cin >> num1 >> num2;
+  sum = solveMeFirst(num1, num2);
+  cout << sum;
   return 0;
 }
 
