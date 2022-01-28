e50f54b0c628798e49c8ba65f8b7a7fbd97e4014
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:01:54 +0530

    Mini-Max Sum

--- HackerRank Solutions/Algorithms/Warmup/Mini-Max Sum/Mini-Max Sum.cpp
+++ HackerRank Solutions/Algorithms/Warmup/Mini-Max Sum/Mini-Max Sum.cpp
@@ -5,7 +5,7 @@ int main() {
     int val;
     long long int sum = 0;
     vector<int> arr;
-    for(int i=0; i<5; i++){
+    for (int i=0; i<5; i++){
         cin >> val;
         sum += val;
         arr.push_back(val);
