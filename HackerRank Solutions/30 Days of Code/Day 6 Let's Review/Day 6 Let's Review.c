a86a1b5702b2940da2d7aee1871b1b31b0c2a944
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:29:14 +0530

    Day 6 Let's Review

--- HackerRank Solutions/30 Days of Code/Day 6 Let's Review/Day 6 Let's Review.c
+++ HackerRank Solutions/30 Days of Code/Day 6 Let's Review/Day 6 Let's Review.c
@@ -8,16 +8,16 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int T; char s[10000];
     scanf("%d", &T);
-    for(int i=0; i<T; i++){
+    for (int i=0; i<T; i++){
         scanf("%s",s);
-        for(int j=0; j<strlen(s); j++){
-            if(j % 2 == 0){
+        for (int j=0; j<strlen(s); j++){
+            if (j % 2 == 0){
                 printf("%c", s[j]);
             }
         }
         printf(" ");
-        for(int j=0; j<strlen(s); j++){
-            if(j % 2 != 0){
+        for (int j=0; j<strlen(s); j++){
+            if (j % 2 != 0){
                 printf("%c", s[j]);
             }
         }
