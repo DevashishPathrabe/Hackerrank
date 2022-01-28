db7bd423c0cb68c1909943b30924772695ce6a73
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:02:13 +0530

    Plus Minus

--- HackerRank Solutions/Algorithms/Warmup/Plus Minus/Plus Minus.java
+++ HackerRank Solutions/Algorithms/Warmup/Plus Minus/Plus Minus.java
@@ -11,20 +11,20 @@ public class Solution {
     // Complete the plusMinus function below.
     static void plusMinus(int[] arr) {
         float positive=0, negative=0, zero=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] > 0){
+        for (int i=0; i<arr.length; i++){
+            if (arr[i] > 0){
                 positive++;
             }
-            if(arr[i] < 0){
+            if (arr[i] < 0){
                 negative++;
             } 
-            if(arr[i] == 0){
+            if (arr[i] == 0){
                 zero++;
             }
         }
-        System.out.println(positive/arr.length);
-        System.out.println(negative/arr.length);
-        System.out.println(zero/arr.length);
+        System.out.println(positive / arr.length);
+        System.out.println(negative / arr.length);
+        System.out.println(zero / arr.length);
     }
 
     private static final Scanner scanner = new Scanner(System.in);
