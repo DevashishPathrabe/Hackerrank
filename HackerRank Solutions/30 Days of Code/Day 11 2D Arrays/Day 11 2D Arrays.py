f0edeb46bc585bed2342cdd38e7a2bb84099a65f
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 19:51:32 +0530

    Day 11 2D Arrays

--- HackerRank Solutions/30 Days of Code/Day 11 2D Arrays/Day 11 2D Arrays.py
+++ HackerRank Solutions/30 Days of Code/Day 11 2D Arrays/Day 11 2D Arrays.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
     sum = 0
-    maxSum = -9*7
+    maxSum = -9 * 7
     for i in range(len(arr) - 2):
         for j in range(len(arr[i]) - 2):
             sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
