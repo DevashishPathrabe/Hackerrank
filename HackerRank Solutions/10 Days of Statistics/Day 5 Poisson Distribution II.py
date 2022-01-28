46dfec926562c2bba83f44766652d907840b7169
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 23:22:16 +0530

    Delete Day 5 Poisson Distribution II.py

--- HackerRank Solutions/10 Days of Statistics/Day 5 Poisson Distribution II.py
+++
@@ -1,6 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-A, B = [float(num) for num in input().split(" ")]
-dailyCostA = 160 + (40 * (A + A**2))
-dailyCostB = 128 + (40 * (B + B**2))
-print(round(dailyCostA, 3))
-print(round(dailyCostB, 3))
