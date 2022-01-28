acad39469dcd39abe65a756ca5367406d5190497
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 21:19:17 +0530

    Day 1 Standard Deviation

--- HackerRank Solutions/10 Days of Statistics/Day 1 Standard Deviation/Day 1 Standard Deviation.java
+++ HackerRank Solutions/10 Days of Statistics/Day 1 Standard Deviation/Day 1 Standard Deviation.java
@@ -9,13 +9,13 @@ public class Solution {
         int n = scan.nextInt();
         int [] arr = new int[n];
         double sum = 0;
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             arr[i] = scan.nextInt();
             sum += arr[i];
         }
         double mean = sum / n;
         double totalSum = 0;
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             totalSum += Math.pow(arr[i] - mean, 2);
         }
         System.out.format("%.1f", Math.sqrt(totalSum / n));
