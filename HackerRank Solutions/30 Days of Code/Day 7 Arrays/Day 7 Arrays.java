313c35e9b48de29f6acd038d91b23aca96766584
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:35:59 +0530

    Day 7 Arrays

--- HackerRank Solutions/30 Days of Code/Day 7 Arrays/Day 7 Arrays.java
+++ HackerRank Solutions/30 Days of Code/Day 7 Arrays/Day 7 Arrays.java
@@ -14,11 +14,11 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i<n; i++){
+        for (int i=0; i<n; i++){
             arr[i] = in.nextInt();
         }
         in.close();      
-        for(int i=n-1; i>=0; i--){
+        for (int i=n-1; i>=0; i--){
             System.out.print(arr[i] + " ");
         }
     }
