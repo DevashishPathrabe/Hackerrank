d24fb574272ee09c39e7fa4c7ef846bd68acf2ab
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 21:17:32 +0530

    Delete Day 1 Standard Deviation.py

--- HackerRank Solutions/10 Days of Statistics/Day 1 Standard Deviation.py
+++
@@ -1,7 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from math import sqrt
-
-n = int(input())
-x = sorted(list(map(int, input().split())))
-u = sum(x)/n
-print(sqrt(sum([(x[i]-u)**2 for i in range(n)])/n))
