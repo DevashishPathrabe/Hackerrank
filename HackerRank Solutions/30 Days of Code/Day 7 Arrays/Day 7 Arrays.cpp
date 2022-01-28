924d17e36ddd34d9945591680f662921e883a028
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:35:49 +0530

    Day 7 Arrays

--- HackerRank Solutions/30 Days of Code/Day 7 Arrays/Day 7 Arrays.cpp
+++ HackerRank Solutions/30 Days of Code/Day 7 Arrays/Day 7 Arrays.cpp
@@ -7,10 +7,10 @@ int main() {
     int N;
     cin >> N;
     vector<int> arr(N);
-    for(int i=0; i<N; i++){
+    for (int i=0; i<N; i++){
         cin >> arr[i];
     }
-    for(int i=N-1; i>=0; i--){
+    for (int i=N-1; i>=0; i--){
         cout << arr[i] << " ";
     }
     return 0;
