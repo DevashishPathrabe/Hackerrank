5d5b9f74de69d64ef59dc38953030e2ecd6f474f
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:11:38 +0530

    Day 2 Operators

--- HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.py
+++ HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.py
@@ -8,8 +8,8 @@ import sys
 
 # Complete the solve function below.
 def solve(meal_cost, tip_percent, tax_percent):
-    tip = meal_cost * tip_percent/100
-    tax = meal_cost * tax_percent/100
+    tip = meal_cost * tip_percent / 100
+    tax = meal_cost * tax_percent / 100
     totalCost = meal_cost + tip + tax
     print(round(totalCost))
 if __name__ == '__main__':
