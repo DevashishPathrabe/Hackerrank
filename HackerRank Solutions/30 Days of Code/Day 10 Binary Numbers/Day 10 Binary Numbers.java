319abaccf23d5ba319cab63d6e2a160fd071395a
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 19:42:15 +0530

    Day 10 Binary Numbers

--- HackerRank Solutions/30 Days of Code/Day 10 Binary Numbers/Day 10 Binary Numbers.java
+++ HackerRank Solutions/30 Days of Code/Day 10 Binary Numbers/Day 10 Binary Numbers.java
@@ -16,13 +16,13 @@ public class Solution {
         int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         int count=0, maxNumberOf1=0;
-        while(n > 0){
-            if(n%2 == 1){
+        while (n > 0){
+            if (n%2 == 1){
                 count++;
             } else{
                 count = 0;
             }
-            if(maxNumberOf1 < count){
+            if (maxNumberOf1 < count){
                 maxNumberOf1 = count;
             }
             n >>= 1;
