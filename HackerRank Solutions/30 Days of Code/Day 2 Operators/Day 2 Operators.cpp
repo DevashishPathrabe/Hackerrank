ecf88203ba3aa7b86357e87e556747d37ba6e2f0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:11:28 +0530

    Day 2 Operators

--- HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.cpp
+++ HackerRank Solutions/30 Days of Code/Day 2 Operators/Day 2 Operators.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 // Complete the solve function below.
 void solve(double meal_cost, int tip_percent, int tax_percent) {
-    double tip = meal_cost * tip_percent/100;
-    double tax = meal_cost * tax_percent/100;
+    double tip = meal_cost * tip_percent / 100;
+    double tax = meal_cost * tax_percent / 100;
     double totalCost = meal_cost + tip + tax;
     cout << round(totalCost) << endl;
 }
