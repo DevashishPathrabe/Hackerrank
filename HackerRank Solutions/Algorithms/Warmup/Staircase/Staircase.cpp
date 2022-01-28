c3739e4a1c4bb01f6cd7d697e8f903c96210e6e0
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-28 14:02:43 +0530

    Staircase

--- HackerRank Solutions/Algorithms/Warmup/Staircase/Staircase.cpp
+++ HackerRank Solutions/Algorithms/Warmup/Staircase/Staircase.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = n; i >= 1; i--) {
-        for (int j = 1; j < i; j++) {
+    for (int i=n; i>=1; i--){
+        for (int j=1; j<i; j++) {
         cout << " ";
         }
-        for (int k = n; k >= i; k--) {
+        for (int k=n; k>=i; k--){
         cout << "#";
         }
         cout << endl;
