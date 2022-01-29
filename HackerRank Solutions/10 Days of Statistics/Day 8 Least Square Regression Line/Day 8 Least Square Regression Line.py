36ef534cd6dff6939a1ceab0a62736087e63417c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 21:39:37 +0530

    Day 8: Least Square Regression Line

--- HackerRank Solutions/10 Days of Statistics/Day 8 Least Square Regression Line/Day 8 Least Square Regression Line.py
+++ HackerRank Solutions/10 Days of Statistics/Day 8 Least Square Regression Line/Day 8 Least Square Regression Line.py
@@ -8,15 +8,15 @@ for i in range(5):
  
 def bValue(x,y):
     n = len(x)
-    xy =[x[i]*y[i] for i in range(n) ]
+    xy = [x[i]*y[i] for i in range(n) ]
     xSquare = [i**2 for i in x]
-    b = (n*(sum(xy)) - ((sum(x)*sum(y)))) / float(((n*sum(xSquare))-sum(x)**2))
+    b = (n * (sum(xy)) - ((sum(x)*sum(y)))) / float(((n*sum(xSquare))-sum(x)**2))
     return b
     
 def abValues(x,y):
-    xMean = sum(x)/float(len(x))
-    yMean = sum(y)/float(len(y))
-    b = bValue(x,y)
+    xMean = sum(x) / float(len(x))
+    yMean = sum(y) / float(len(y))
+    b = bValue(x, y)
     a = yMean - (b * xMean)
     return a, b
 
