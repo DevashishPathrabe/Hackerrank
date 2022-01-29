c59f075208c382295a36b7e4178163c04a6134f1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 12:12:12 +0530

    Day 4 Geometric Distribution I

--- HackerRank Solutions/10 Days of Statistics/Day 4 Geometric Distribution I/Day 4 Geometric Distribution I.java
+++ HackerRank Solutions/10 Days of Statistics/Day 4 Geometric Distribution I/Day 4 Geometric Distribution I.java
@@ -5,7 +5,7 @@ public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        double defP = 1d/3;
+        double defP = 1d / 3;
         System.out.println(Math.round(1000 * Math.pow(1-defP, 4) * defP) / 1000d);
     }
 }
