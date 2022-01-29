2f8164c33061302b9bb851c6c27204b5fb6e7317
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 12:28:38 +0530

    Delete Day 5 Poisson Distribution I.py

--- HackerRank Solutions/10 Days of Statistics/Day 5 Poisson Distribution I.py
+++
@@ -1,6 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import math
-lam = float(input())
-k = int(input())
-res = ((math.pow(lam,k) * math.exp(-lam))/math.factorial(k))
-print("{:.3f}".format(res))
