1dfe836aaf667f8ea6e6ee96dae41acec29b15f1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 20:21:05 +0530

    Day 19: Interfaces

--- HackerRank Solutions/30 Days of Code/Day 19 Interfaces/Day 19 Interfaces.py
+++ HackerRank Solutions/30 Days of Code/Day 19 Interfaces/Day 19 Interfaces.py
@@ -6,7 +6,7 @@ class Calculator(AdvancedArithmetic):
     def divisorSum(self, n):
         output = 0
         for i in range(1, n+1):
-            if(n % i == 0):
+            if (n % i == 0):
                 output += i
         return output
 
