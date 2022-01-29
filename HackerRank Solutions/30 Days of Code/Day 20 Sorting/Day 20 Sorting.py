7af6d213f1eb94d598bfe13de18772646fb3a817
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 20:22:10 +0530

    Day 20: Sorting

--- HackerRank Solutions/30 Days of Code/Day 20 Sorting/Day 20 Sorting.py
+++ HackerRank Solutions/30 Days of Code/Day 20 Sorting/Day 20 Sorting.py
@@ -5,7 +5,7 @@ import sys
 n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 # Write Your Code Here
-def swap(x,y):
+def swap(x, y):
     temp = x
     x = y
     y = temp
@@ -14,10 +14,10 @@ numSwaps = 0
 for i in range(n-1):
     count = 0
     for j in range(n-1):
-        if(a[j] > a[j+1]):
+        if (a[j] > a[j+1]):
             a[j], a[j+1] = swap(a[j], a[j+1])
             count = count+1
-    if(count == 0):
+    if (count == 0):
         break;
     else :
         numSwaps = numSwaps + count
