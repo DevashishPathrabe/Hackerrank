b5dc6ae558026f764160701d1a23b3cddd572b37
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 20:34:19 +0530

    Day 26: Nested Logic

--- HackerRank Solutions/30 Days of Code/Day 26 Nested Logic/Day 26 Nested Logic.py
+++ HackerRank Solutions/30 Days of Code/Day 26 Nested Logic/Day 26 Nested Logic.py
@@ -2,11 +2,11 @@
 dateActually, monthActually, yearActually = list(map(int, input().split()))
 dateExpected, monthExpected, yearExpected = list(map(int, input().split()))
 fine = 0
-if(yearActually > yearExpected):
+if (yearActually > yearExpected):
     fine = 10000
-elif(yearActually == yearExpected):
-    if(monthActually > monthExpected):
+elif (yearActually == yearExpected):
+    if (monthActually > monthExpected):
         fine = (monthActually - monthExpected) * 500
-    elif(monthActually == monthExpected and dateActually > dateExpected):
+    elif (monthActually == monthExpected and dateActually > dateExpected):
         fine = (dateActually - dateExpected) * 15
 print(fine)
