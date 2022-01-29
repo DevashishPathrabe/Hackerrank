416734f313c41b3c840dc524fddd43a305f023f3
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 20:19:41 +0530

    Day 18: Queues and Stacks

--- HackerRank Solutions/30 Days of Code/Day 18 Queues and Stacks/Day 18 Queues and Stacks.py
+++ HackerRank Solutions/30 Days of Code/Day 18 Queues and Stacks/Day 18 Queues and Stacks.py
@@ -5,16 +5,16 @@ class Solution:
     def __init__(self):
         self.stack = []
         self.queue = []
-    def pushCharacter(self , ch):
+    def pushCharacter(self, ch):
         self.stack.append(ch)
-    def enqueueCharacter(self , ch):
+    def enqueueCharacter(self, ch):
         self.queue.append(ch)
     def popCharacter(self):
         return self.stack.pop()
     def dequeueCharacter(self):
         ch = self.queue[0]
         self.queue = self.queue[1:]
-        return ch       
+        return ch
 # read the string s
 s=input()
 #Create the Solution class object
