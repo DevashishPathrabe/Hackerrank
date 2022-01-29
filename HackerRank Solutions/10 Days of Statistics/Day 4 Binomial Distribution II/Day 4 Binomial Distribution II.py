9b8000f6583e8235cf67d4a32c3fea55bd739f9d
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 11:54:29 +0530

    Day 4 Binomial Distribution II

--- HackerRank Solutions/10 Days of Statistics/Day 4 Binomial Distribution II/Day 4 Binomial Distribution II.py
+++ HackerRank Solutions/10 Days of Statistics/Day 4 Binomial Distribution II/Day 4 Binomial Distribution II.py
@@ -4,8 +4,8 @@ from math import factorial
 p = 12/100.0
 q = 1-p
 N = 10
-B0 = factorial(N)/(factorial(0)*factorial(N-0)) * p**0 * q**(N-0)
-B1 = factorial(N)/(factorial(1)*factorial(N-1)) * p**1 * q**(N-1)
-B2 = factorial(N)/(factorial(2)*factorial(N-2)) * p**2 * q**(N-2)
-print(round(B0+B1+B2,3))
-print(round(1-(B0+B1),3))
+B0 = factorial(N) / (factorial(0)*factorial(N-0)) * p**0 * q**(N-0)
+B1 = factorial(N) / (factorial(1)*factorial(N-1)) * p**1 * q**(N-1)
+B2 = factorial(N) / (factorial(2)*factorial(N-2)) * p**2 * q**(N-2)
+print(round(B0+B1+B2, 3))
+print(round(1-(B0+B1), 3))
