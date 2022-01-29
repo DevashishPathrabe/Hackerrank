07d0fe76fa7336000db2a8a2374f8cf68e4c9f43
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 12:09:33 +0530

    Delete Day 4 Geometric Distribution I.py

--- HackerRank Solutions/10 Days of Statistics/Day 4 Geometric Distribution I.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-in0 = list( map( float, input().strip().split(" ") ) )
-n = int(input())
-
-pf = in0[0] / in0[1]
-
-p = (1-pf)**(n-1) * pf
-
-print("%.3f" % p)
