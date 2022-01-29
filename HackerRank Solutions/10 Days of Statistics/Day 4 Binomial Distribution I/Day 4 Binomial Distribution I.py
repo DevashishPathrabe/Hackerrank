23fffbf2fc6c370c65514eb3d45f4a4d6d565a8c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-01-29 11:54:19 +0530

    Day 4 Binomial Distribution I

--- HackerRank Solutions/10 Days of Statistics/Day 4 Binomial Distribution I/Day 4 Binomial Distribution I.py
+++ HackerRank Solutions/10 Days of Statistics/Day 4 Binomial Distribution I/Day 4 Binomial Distribution I.py
@@ -2,7 +2,7 @@
 import math
 
 def biniomialDistribution(x, n, p):
-    b = (math.factorial(n)/(math.factorial(x)*math.factorial(n-x)))*(p**x)*((1-p)**(n-x))
+    b = (math.factorial(n) / (math.factorial(x)*math.factorial(n-x)))*(p**x)*((1-p)**(n-x))
     return(b)
 b, p, n = 0, 1.09/2.09, 6
 for i in range(3,7):
