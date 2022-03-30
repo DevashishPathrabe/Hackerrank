4654ec6b22acc15f04bc5403d5441e7f3b66ad55
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-03-30 19:11:17 +0530

    Day 15 Linked List

---
+++ HackerRank Solutions/30Days of Code/Day 15 Linked List/Day 15 Linked List.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self, head):
+        current = head
+        while current:
+            print(current.data, end=' ')
+            current = current.next
+
+    def insert(self, head, data): 
+    #Complete this method
+        newNode = Node(data)
+        if (head == None):
+            return newNode
+        else:
+            currentNode = head;
+            while (currentNode.next != None):
+                currentNode = currentNode.next;
+            currentNode.next = newNode
+            return head
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
