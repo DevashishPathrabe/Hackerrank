9578098e735f66c1107b138cf433ec9d22817655
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-04-03 14:25:46 +0530

    Day 3 Intro to Conditional Statements

---
+++ HackerRank Solutions/30 Days of Code/Day 3 Intro to Conditional Statements/Day 3 Intro to Conditional Statements.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    if (N%2 == 1){
+        cout << "Weird" << endl;
+    }
+    if (N%2 == 0 && 2 <= N && N <= 5){
+        cout << "Not Weird" << endl;
+    }
+    if (N%2 == 0 && 6 <= N && N <= 20){
+        cout << "Weird" << endl;
+    }
+    if (N%2 == 0 && N > 20){
+        cout << "Not Weird" << endl;
+    }
+    return 0;
+}
