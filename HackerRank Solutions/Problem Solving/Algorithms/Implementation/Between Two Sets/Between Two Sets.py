57cc44aadb9553fb80bca51b0b3f01bea9c4ea66
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-22 10:12:07 +0530

    Between Two Sets

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Between Two Sets/Between Two Sets.py
@@ -0,0 +1,46 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    # Write your code here
+    maxA = max(a)
+    minB = min(b)
+    count = 0
+    for num in range(maxA, minB + 1):
+        left = all([num % numA == 0 for numA in a])
+        right = all([numB % num == 0 for numB in b])
+        count += left * right
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
