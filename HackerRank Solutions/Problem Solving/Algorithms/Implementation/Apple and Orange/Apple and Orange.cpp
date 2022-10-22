290c154f9f571154519da9dfda606b15b10e0bd1
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-22 10:11:35 +0530

    Append and Delete

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Apple and Orange/Apple and Orange.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int s, t, a, b, m, n, apple, totalapples = 0, orange, totaloranges = 0;
+    cin >> s >> t >> a >> b >> m >> n;
+    for (int i=0; i<m; i++){
+        cin >> apple;
+        if (a+apple >= s && a+apple <= t){
+            totalapples++;
+        }
+    }
+    for (int i=0; i<n; i++){
+        cin >> orange;
+        if (b+orange >= s && b+orange <= t){
+            totaloranges++;
+        }
+    }
+    cout << totalapples << endl << totaloranges << endl;
+    return 0;
+}
