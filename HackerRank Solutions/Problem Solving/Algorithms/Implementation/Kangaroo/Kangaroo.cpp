66c38da4762da9096db338cf0fa7666d4959e62c
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-23 09:34:52 +0530

    Kangaroo

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Kangaroo/Kangaroo.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x1, v1, x2, v2;
+    cin >> x1 >> v1 >> x2 >> v2;
+    if ((v1>v2 && x1<x2) || (v1<v2 &&x1>x2)){
+        for (int i=0; i<10000; i++){
+            x1 += v1;
+            x2 += v2;
+            if (x1 == x2){
+                cout << "YES" << endl;
+                return 0;
+            }
+        }
+        cout << "NO" << endl;
+        return 0;
+    }
+    else{
+        cout << "NO" << endl;
+        return 0;
+    }
+}
