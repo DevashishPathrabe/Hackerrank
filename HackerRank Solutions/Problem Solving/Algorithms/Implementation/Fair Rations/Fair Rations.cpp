9a5d93931a5b47b7d7c9a19ca93f801709184d58
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-23 09:32:42 +0530

    Fair Rations

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Fair Rations/Fair Rations.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N; 
+    cin >> N;
+    int B[N];
+    for (int i=0; i<N; i++){
+        cin >> B[i];
+    }
+    int numberOfLoaves = 0;
+    for (int i=0; i<N-1; i++){
+        if (B[i]%2 != 0){
+            B[i] += 1;
+            B[i+1] += 1; 
+            numberOfLoaves += 2;
+        }
+    }
+    for (int i=0; i<N; i++){
+        if (B[i]%2 != 0) {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << numberOfLoaves << endl;
+    return 0;
+}
