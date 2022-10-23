4be70845d3fe2a60818687ea09aba9c63f948a99
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-23 09:27:03 +0530

    Bon Appétit

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Bon Appétit/Bon Appétit.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, bActual=0, bCharged, bill;
+    cin >> n >> k;
+    for (int i=0; i<n; i++){
+        cin >> bill;
+        if (i != k){
+            bActual += bill;
+        }
+    }
+    bActual = bActual/2;
+    cin >> bCharged;
+    if (bCharged == bActual){
+        cout << "Bon Appetit" << endl;
+    } else{
+        cout << bCharged - bActual << endl;
+    }
+    return 0;
+}
