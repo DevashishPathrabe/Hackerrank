7d3ddbe3025d20ec3acbaec238c1eac653f3e0af
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-23 09:32:12 +0530

    Drawing Book

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Drawing Book/Drawing Book.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, p, noOfPages;
+    cin >> n >> p;
+    noOfPages = (n / 2) - (p / 2);
+    if (noOfPages > p / 2){
+        noOfPages = p / 2;
+    }
+    cout << noOfPages << endl;
+    return 0;
+}
