7fcfaeeb86079b94bef07a2024bec8f740cf8432
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-24 09:25:17 +0530

    Caesar Cipher

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Strings/Caesar Cipher/Caesar Cipher.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    result = ""
+    for i in s:
+        if i.islower():
+            result += chr((ord(i)-97+k)%26+97)
+        elif i.isupper():
+            result += chr((ord(i)-65+k)%26+65)
+        else:
+            result += i
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
