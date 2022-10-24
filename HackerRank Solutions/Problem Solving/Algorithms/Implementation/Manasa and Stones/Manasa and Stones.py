77e9798cb58c7db4d3083538c7c006146b0ec689
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-24 09:18:17 +0530

    Manasa and Stones

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Implementation/Manasa and Stones/Manasa and Stones.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stones' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER a
+#  3. INTEGER b
+#
+
+def stones(n, a, b):
+    # Write your code here
+    return sorted(set([(n-1)*min(a, b) + x*abs(a-b) for x in range(n)]))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        a = int(input().strip())
+
+        b = int(input().strip())
+
+        result = stones(n, a, b)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
