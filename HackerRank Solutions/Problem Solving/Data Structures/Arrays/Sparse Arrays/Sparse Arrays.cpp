a59272e97386ad1c64eaaee3a2542e19c5204d23
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-25 10:13:07 +0530

    Sparse Arrays

---
+++ HackerRank Solutions/Problem Solving/Data Structures/Arrays/Sparse Arrays/Sparse Arrays.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string,int>m;
+    int n,q;
+    string str; 
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> str;
+        m[str]++;
+    }
+    cin >> q;
+    for(int i=0; i<q; i++){
+        cin >> str;
+        cout << m[str] << endl;
+    }
+    return 0;
+}
