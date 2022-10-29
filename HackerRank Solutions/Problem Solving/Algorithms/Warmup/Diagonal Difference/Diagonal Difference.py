a6af2c0c9aace62771cd8f20b9d5f9789ce7d148
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-29 09:13:51 +0530

    Diagonal Difference

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Warmup/Diagonal Difference/Diagonal Difference.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    diagonal1Sum, diagonal2Sum = 0, 0
+    for i in range(len(arr)):
+        diagonal1Sum += arr[i][i]
+        diagonal2Sum += arr[i][len(arr)-i-1]
+    return abs(diagonal1Sum - diagonal2Sum)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
