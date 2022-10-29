ad72dc47ce2f8bff3daffc9546eecf61205e9c66
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-10-29 16:07:05 +0530

    Number Groups

---
+++ HackerRank Solutions/Problem Solving/Mathematics/Algebra/Number Groups/Number Groups.c
@@ -0,0 +1,68 @@
+#include <assert.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+// Complete the sumOfGroup function below.
+long sumOfGroup(int k) {
+    // Return the sum of the elements of the k'th group.
+    return (long)k*k*k;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* k_endptr;
+    char* k_str = readline();
+    int k = strtol(k_str, &k_endptr, 10);
+
+    if (k_endptr == k_str || *k_endptr != '\0') { exit(EXIT_FAILURE); }
+
+    long answer = sumOfGroup(k);
+
+    fprintf(fptr, "%ld\n", answer);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) { break; }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') { break; }
+
+        size_t new_length = alloc_length << 1;
+        data = realloc(data, new_length);
+
+        if (!data) { break; }
+
+        alloc_length = new_length;
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+    }
+
+    data = realloc(data, data_length);
+
+    return data;
+}
