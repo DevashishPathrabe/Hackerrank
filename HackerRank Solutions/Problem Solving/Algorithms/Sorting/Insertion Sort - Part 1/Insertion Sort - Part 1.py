4b6aba9b17c93530f4d859d04ec258c5fb363000
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-06 12:10:10 +0530

    Insertion Sort - Part 1

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Sorting/Insertion Sort - Part 1/Insertion Sort - Part 1.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
+def insertionSort1(n, arr):
+    # Write your code here
+    for i in range((n-1), 0, -1):
+        if arr[i] < arr[i-1]:
+            temp = arr[i]
+            arr[i] = arr[i-1]
+            print(*arr)
+            arr[i-1] = temp
+    print(*arr)
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
