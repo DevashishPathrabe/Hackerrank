446f5dee608138e4e3c25177443de88707431ec5
DevashishPathrabe <67232653+DevashishPathrabe@users.noreply.github.com>
2022-11-06 12:10:48 +0530

    Quicksort 1 - Partition

---
+++ HackerRank Solutions/Problem Solving/Algorithms/Sorting/Quicksort 1 - Partition/Quicksort 1 - Partition.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    # Write your code here
+    pivot = arr[0];
+    for i in range(1, len(arr)):
+        if pivot > arr[i]:
+            for j in range(i, 0, -1):
+                temp = arr[j]
+                arr[j] = arr[j-1]
+                arr[j-1] = temp
+    return arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
